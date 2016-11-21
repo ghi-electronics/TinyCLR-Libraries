81dde176504a0de52e4fc40fb5ab813bd37f4474
John Brochue <john.brochue@ghielectronics.com>
2016-11-21 14:32:54 -0500

    Remade the native project.

---
+++ GHIElectronics.TinyCLR.Native/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GHIElectronics.TinyCLR.Native")]
+[assembly: AssemblyDescription("TinyCLR native library.")]
+[assembly: AssemblyCompany("GHI Electronics, LLC")]
+[assembly: AssemblyProduct("TinyCLR")]
+[assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2016")]
+[assembly: ComVisible(false)]
+[assembly: Guid("9B7036C3-147F-4311-911C-8906629A3121")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
