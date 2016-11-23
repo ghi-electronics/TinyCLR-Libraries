9ec3b631dbf49efc9e095df37eea1c3d0d506224
John Brochue <john.brochue@ghielectronics.com>
2016-11-23 14:17:23 -0500

    Added a basic debug write class.

---
+++ GHIElectronics.TinyCLR.Core/Diagnostics/Debug.cs
@@ -0,0 +1,9 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace System.Diagnostics {
+    public static class Debug {
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        [Conditional("DEBUG")]
+        public static extern void WriteLine(string message);
+    }
+}
