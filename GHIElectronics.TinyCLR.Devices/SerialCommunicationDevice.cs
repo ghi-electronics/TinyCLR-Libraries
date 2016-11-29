8c683a92952ce5093ad5b6b6a200b2e739f18378
John Brochue <john.brochue@ghielectronics.com>
2016-11-29 13:52:25 -0500

    Added serial device skeleton.

---
+++ GHIElectronics.TinyCLR.Devices/SerialCommunicationDevice.cs
@@ -0,0 +1,120 @@
+﻿using GHIElectronics.TinyCLR.Storage.Streams;
+using System;
+
+namespace GHIElectronics.TinyCLR.Devices.SerialCommunication {
+    public delegate void ErrorReceivedDelegate(SerialDevice sender, ErrorReceivedEventArgs e);
+    public delegate void PinChangedDelegate(SerialDevice sender, PinChangedEventArgs e);
+
+    public class SerialDevice : IDisposable {
+        private bool disposed;
+
+        public bool BreakSignalState { get { throw new NotSupportedException(); } }
+        public uint BytesReceived { get { throw new NotSupportedException(); } }
+        public bool CarrierDetectState { get { throw new NotSupportedException(); } }
+        public bool ClearToSendState { get { throw new NotSupportedException(); } }
+        public bool DataSetReadyState { get { throw new NotSupportedException(); } }
+        public bool IsDataTerminalReadyEnabled { get { throw new NotSupportedException(); } set { throw new NotSupportedException(); } }
+        public bool IsRequestToSendEnabled { get { throw new NotSupportedException(); } set { throw new NotSupportedException(); } }
+        public TimeSpan ReadTimeout { get { throw new NotSupportedException(); } set { throw new NotSupportedException(); } }
+        public ushort UsbProductId { get { throw new NotSupportedException(); } }
+        public ushort UsbVendorId { get { throw new NotSupportedException(); } }
+        public TimeSpan WriteTimeout { get { throw new NotSupportedException(); } set { throw new NotSupportedException(); } }
+
+        public string PortName { get; private set; }
+        public uint BaudRate { get; set; }
+        public ushort DataBits { get; set; }
+        public SerialParity Parity { get; set; }
+        public SerialHandshake Handshake { get; set; }
+        public SerialStopBitCount StopBits { get; set; }
+
+        public IInputStream InputStream { get; private set; }
+        public IOutputStream OutputStream { get; private set; }
+
+        public event ErrorReceivedDelegate ErrorReceived { add { throw new NotSupportedException(); } remove { throw new NotSupportedException(); } }
+        public event PinChangedDelegate PinChanged { add { throw new NotSupportedException(); } remove { throw new NotSupportedException(); } }
+
+        public static string GetDeviceSelector() => string.Empty;
+        public static string GetDeviceSelector(string portName) => string.Empty + portName;
+        public static string GetDeviceSelectorFromUsbVidPid(ushort vendorId, ushort productId) { throw new NotSupportedException(); }
+
+        public static SerialDevice FromIdAsync(string deviceId) {
+            var device = new SerialDevice();
+            var stream = new Stream(device);
+
+            device.PortName = deviceId;
+            device.BaudRate = 9600;
+            device.DataBits = 8;
+            device.Parity = SerialParity.None;
+            device.StopBits = SerialStopBitCount.One;
+            device.InputStream = stream;
+            device.OutputStream = stream;
+
+            device.disposed = false;
+
+            return device;
+        }
+
+        public void Dispose() => this.Dispose(true);
+
+        protected void Dispose(bool disposing) {
+            if (this.disposed)
+                return;
+
+            if (disposing) {
+                (this.InputStream as Stream)?.ParentDispose();
+                this.InputStream = null;
+                this.OutputStream = null;
+            }
+
+            this.disposed = true;
+        }
+
+        ~SerialDevice() {
+            GC.SuppressFinalize(this);
+            this.Dispose(false);
+        }
+
+        private class Stream : IInputStream, IOutputStream, IDisposable {
+            private readonly SerialDevice parent;
+            private bool opened;
+
+            public Stream(SerialDevice parent) {
+                this.parent = parent;
+                this.opened = false;
+            }
+
+            public void Dispose() => this.parent.Dispose();
+
+            internal void ParentDispose() {
+
+            }
+
+            public bool FlushAsync() {
+                return false;
+            }
+
+            public uint ReadAsync(IBuffer buffer, uint count, InputStreamOptions options) {
+                if (count > buffer.Capacity) throw new InvalidOperationException($"{nameof(count)} is more than the capacity of {nameof(buffer)}.");
+                if (this.parent.disposed) throw new ObjectDisposedException();
+
+                if (!this.opened)
+                    this.Open();
+
+                return 0;
+            }
+
+            public uint WriteAsync(IBuffer buffer) {
+                if (this.parent.disposed) throw new ObjectDisposedException();
+
+                if (!this.opened)
+                    this.Open();
+
+                return 0;
+            }
+
+            private void Open() {
+
+            }
+        }
+    }
+}
---
+++ GHIElectronics.TinyCLR.Devices/SerialCommunicationEnums.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace GHIElectronics.TinyCLR.Devices.SerialCommunication {
+    public enum SerialError {
+        Frame,
+        BufferOverrun,
+        ReceiveFull,
+        ReceiveParity,
+        TransmitFull
+    }
+
+    public enum SerialHandshake {
+        None,
+        RequestToSend,
+        XOnXOff,
+        RequestToSendXOnXOff
+    }
+
+    public enum SerialParity {
+        None,
+        Odd,
+        Even,
+        Mark,
+        Space
+    }
+
+    public enum SerialPinChange {
+        BreakSignal,
+        CarrierDetect,
+        ClearToSend,
+        DataSetReady,
+        RingIndicator
+    }
+
+    public enum SerialStopBitCount {
+        One,
+        OnePointFive,
+        Two
+    }
+
+    public class ErrorReceivedEventArgs {
+        public SerialError Error { get; }
+
+        internal ErrorReceivedEventArgs(SerialError error) {
+            this.Error = error;
+        }
+    }
+
+    public class PinChangedEventArgs {
+        public SerialPinChange PinChange { get; }
+
+        internal PinChangedEventArgs(SerialPinChange pinChange) {
+            this.PinChange = pinChange;
+        }
+    }
+}
+
+namespace GHIElectronics.TinyCLR.Storage.Streams {
+    public enum InputStreamOptions {
+        None = 0,
+        Partial = 1,
+        ReadAhead = 2
+    }
+
+    public interface IOutputStream : IDisposable {
+        bool FlushAsync();
+        uint WriteAsync(IBuffer buffer);
+    }
+
+    public interface IInputStream : IDisposable {
+        uint ReadAsync(IBuffer buffer, uint count, InputStreamOptions options);
+    }
+
+    public interface IBuffer {
+        uint Capacity { get; }
+        uint Length { get; set; }
+    }
+
+    public class Buffer : IBuffer {
+        private uint length;
+        private byte[] data;
+
+        internal byte[] Data => this.data;
+        public uint Capacity => this.length;
+
+        public uint Length {
+            get {
+                return this.length;
+            }
+            set {
+                this.length = value;
+
+                var newBuffer = new byte[this.length];
+
+                Array.Copy(this.data, newBuffer, (int)this.length);
+
+                this.data = newBuffer;
+            }
+        }
+
+        public Buffer(byte[] data) {
+            if (data == null) throw new ArgumentNullException(nameof(data));
+
+            this.data = data;
+            this.length = (uint)data.Length;
+        }
+
+        public Buffer(uint length) {
+            this.data = new byte[length];
+            this.length = length;
+        }
+    }
+}
