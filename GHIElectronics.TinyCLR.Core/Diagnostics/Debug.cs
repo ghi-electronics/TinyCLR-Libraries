9973cb30118d995f43ef3ffc806a7f20f0915cd7
John Brochue <john.brochue@ghielectronics.com>
2017-03-16 14:33:01 -0400

    Added basic Trace and assert to Debug.

--- GHIElectronics.TinyCLR.Core/Diagnostics/Debug.cs
+++ GHIElectronics.TinyCLR.Core/Diagnostics/Debug.cs
@@ -3,7 +3,30 @@
 namespace System.Diagnostics {
     public static class Debug {
         [MethodImpl(MethodImplOptions.InternalCall)]
+        internal static extern void WriteLineNative(string message);
+
         [Conditional("DEBUG")]
-        public static extern void WriteLine(string message);
+        public static void WriteLine(string message) => Debug.WriteLineNative(message);
+
+        [Conditional("DEBUG")]
+        public static void WriteLineIf(bool condition, string message) {
+            if (condition)
+                Debug.WriteLine(message);
+        }
+
+        [Conditional("DEBUG")]
+        public static void Assert(bool condition) => Debug.Assert(condition, string.Empty, string.Empty);
+
+        [Conditional("DEBUG")]
+        public static void Assert(bool condition, string message) => Debug.Assert(condition, message, string.Empty);
+
+        [Conditional("DEBUG")]
+        public static void Assert(bool condition, string message, string detailedMessage) {
+            if (!condition) {
+                Debug.WriteLineIf(message != string.Empty, message);
+                Debug.WriteLineIf(detailedMessage != string.Empty, detailedMessage);
+                Debugger.Break();
+            }
+        }
     }
 }
---
+++ GHIElectronics.TinyCLR.Core/Diagnostics/Trace.cs
@@ -0,0 +1,27 @@
+﻿namespace System.Diagnostics {
+    public static class Trace {
+        [Conditional("TRACE")]
+        public static void WriteLine(string message) => Debug.WriteLineNative(message);
+
+        [Conditional("TRACE")]
+        public static void WriteLineIf(bool condition, string message) {
+            if (condition)
+                Trace.WriteLine(message);
+        }
+
+        [Conditional("TRACE")]
+        public static void Assert(bool condition) => Trace.Assert(condition, string.Empty, string.Empty);
+
+        [Conditional("TRACE")]
+        public static void Assert(bool condition, string message) => Trace.Assert(condition, message, string.Empty);
+
+        [Conditional("TRACE")]
+        public static void Assert(bool condition, string message, string detailedMessage) {
+            if (!condition) {
+                Trace.WriteLineIf(message != string.Empty, message);
+                Trace.WriteLineIf(detailedMessage != string.Empty, detailedMessage);
+                Debugger.Break();
+            }
+        }
+    }
+}
