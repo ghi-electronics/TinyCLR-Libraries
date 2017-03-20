03a21e0a9cd5b4c45548d2d2c1a152d6651274de
John Brochue <john.brochue@ghielectronics.com>
2017-03-20 14:27:59 -0400

    Added FormattableString.

---
+++ GHIElectronics.TinyCLR.Core/FormatException.cs
@@ -0,0 +1,8 @@
+﻿namespace System {
+    [Serializable]
+    public class FormatException : SystemException {
+        public FormatException() { }
+        public FormatException(string message) : base(message) { }
+        public FormatException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
---
+++ GHIElectronics.TinyCLR.Core/FormattableString.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace System {
+    public abstract class FormattableString : IFormattable {
+        public abstract string Format { get; }
+        public abstract object[] GetArguments();
+        public abstract int ArgumentCount { get; }
+        public abstract object GetArgument(int index);
+        public abstract string ToString(IFormatProvider formatProvider);
+
+        string IFormattable.ToString(string ignored, IFormatProvider formatProvider) => this.ToString(formatProvider);
+
+        public override string ToString() => this.ToString(CultureInfo.CurrentCulture);
+
+        public static string Invariant(FormattableString formattable) {
+            if (formattable == null) throw new ArgumentNullException(nameof(formattable));
+
+            return formattable.ToString(CultureInfo.InvariantCulture);
+        }
+    }
+}
