cb91b8e644a08ec318065aa90be23a5ab4daa2bd
John Brochue <john.brochue@ghielectronics.com>
2017-03-22 14:59:04 -0400

    Added G400 to pins.

---
+++ GHIElectronics.TinyCLR.Pins/G400D.cs
@@ -0,0 +1,275 @@
+﻿namespace GHI.Pins {
+    /// <summary>Board definition for the G400.</summary>
+    public static class G400D {
+        /// <summary>GPIO pin definitions.</summary>
+        public static class GpioPin {
+            /// <summary>GPIO pin.</summary>
+            public const int PA0 = 0;
+            /// <summary>GPIO pin.</summary>
+            public const int PA1 = 1;
+            /// <summary>GPIO pin.</summary>
+            public const int PA2 = 2;
+            /// <summary>GPIO pin.</summary>
+            public const int PA3 = 3;
+            /// <summary>GPIO pin.</summary>
+            public const int PA4 = 4;
+            /// <summary>GPIO pin.</summary>
+            public const int PA5 = 5;
+            /// <summary>GPIO pin.</summary>
+            public const int PA6 = 6;
+            /// <summary>GPIO pin.</summary>
+            public const int PA7 = 7;
+            /// <summary>GPIO pin.</summary>
+            public const int PA8 = 8;
+            /// <summary>GPIO pin.</summary>
+            public const int PA9 = 9;
+            /// <summary>GPIO pin.</summary>
+            public const int PA10 = 10;
+            /// <summary>GPIO pin.</summary>
+            public const int PA15 = 15;
+            /// <summary>GPIO pin.</summary>
+            public const int PA16 = 16;
+            /// <summary>GPIO pin.</summary>
+            public const int PA17 = 17;
+            /// <summary>GPIO pin.</summary>
+            public const int PA18 = 18;
+            /// <summary>GPIO pin.</summary>
+            public const int PA19 = 19;
+            /// <summary>GPIO pin.</summary>
+            public const int PA20 = 20;
+            /// <summary>GPIO pin.</summary>
+            public const int PA21 = 21;
+            /// <summary>GPIO pin.</summary>
+            public const int PA22 = 22;
+            /// <summary>GPIO pin.</summary>
+            public const int PA23 = 23;
+            /// <summary>GPIO pin.</summary>
+            public const int PA24 = 24;
+            /// <summary>GPIO pin.</summary>
+            public const int PA25 = 25;
+            /// <summary>GPIO pin.</summary>
+            public const int PA26 = 26;
+            /// <summary>GPIO pin.</summary>
+            public const int PA27 = 27;
+            /// <summary>GPIO pin.</summary>
+            public const int PA28 = 28;
+            /// <summary>GPIO pin.</summary>
+            public const int PA29 = 29;
+            /// <summary>GPIO pin.</summary>
+            public const int PA30 = 30;
+            /// <summary>GPIO pin.</summary>
+            public const int PA31 = 31;
+            /// <summary>GPIO pin.</summary>
+            public const int PB8 = 8 + 32;
+            /// <summary>GPIO pin.</summary>
+            public const int PB11 = 11 + 32;
+            /// <summary>GPIO pin.</summary>
+            public const int PB12 = 12 + 32;
+            /// <summary>GPIO pin.</summary>
+            public const int PB13 = 13 + 32;
+            /// <summary>GPIO pin.</summary>
+            public const int PB14 = 14 + 32;
+            /// <summary>GPIO pin.</summary>
+            public const int PB15 = 15 + 32;
+            /// <summary>GPIO pin.</summary>
+            public const int PB16 = 16 + 32;
+            /// <summary>GPIO pin.</summary>
+            public const int PB17 = 17 + 32;
+            /// <summary>GPIO pin.</summary>
+            public const int PB18 = 18 + 32;
+            /// <summary>GPIO pin.</summary>
+            public const int PC0 = 0 + 64;
+            /// <summary>GPIO pin.</summary>
+            public const int PC1 = 1 + 64;
+            /// <summary>GPIO pin.</summary>
+            public const int PC2 = 2 + 64;
+            /// <summary>GPIO pin.</summary>
+            public const int PC3 = 3 + 64;
+            /// <summary>GPIO pin.</summary>
+            public const int PC4 = 4 + 64;
+            /// <summary>GPIO pin.</summary>
+            public const int PC5 = 5 + 64;
+            /// <summary>GPIO pin.</summary>
+            public const int PC6 = 6 + 64;
+            /// <summary>GPIO pin.</summary>
+            public const int PC7 = 7 + 64;
+            /// <summary>GPIO pin.</summary>
+            public const int PC8 = 8 + 64;
+            /// <summary>GPIO pin.</summary>
+            public const int PC9 = 9 + 64;
+            /// <summary>GPIO pin.</summary>
+            public const int PC10 = 10 + 64;
+            /// <summary>GPIO pin.</summary>
+            public const int PC11 = 11 + 64;
+            /// <summary>GPIO pin.</summary>
+            public const int PC12 = 12 + 64;
+            /// <summary>GPIO pin.</summary>
+            public const int PC13 = 13 + 64;
+            /// <summary>GPIO pin.</summary>
+            public const int PC14 = 14 + 64;
+            /// <summary>GPIO pin.</summary>
+            public const int PC15 = 15 + 64;
+            /// <summary>GPIO pin.</summary>
+            public const int PC16 = 16 + 64;
+            /// <summary>GPIO pin.</summary>
+            public const int PC17 = 17 + 64;
+            /// <summary>GPIO pin.</summary>
+            public const int PC18 = 18 + 64;
+            /// <summary>GPIO pin.</summary>
+            public const int PC19 = 19 + 64;
+            /// <summary>GPIO pin.</summary>
+            public const int PC20 = 20 + 64;
+            /// <summary>GPIO pin.</summary>
+            public const int PC21 = 21 + 64;
+            /// <summary>GPIO pin.</summary>
+            public const int PC22 = 22 + 64;
+            /// <summary>GPIO pin.</summary>
+            public const int PC23 = 23 + 64;
+            /// <summary>GPIO pin.</summary>
+            public const int PC24 = 24 + 64;
+            /// <summary>GPIO pin.</summary>
+            public const int PC26 = 26 + 64;
+            /// <summary>GPIO pin.</summary>
+            public const int PC27 = 27 + 64;
+            /// <summary>GPIO pin.</summary>
+            public const int PC28 = 28 + 64;
+            /// <summary>GPIO pin.</summary>
+            public const int PC29 = 29 + 64;
+            /// <summary>GPIO pin.</summary>
+            public const int PC30 = 30 + 64;
+            /// <summary>GPIO pin.</summary>
+            public const int PC31 = 31 + 64;
+            /// <summary>GPIO pin.</summary>
+            public const int PD0 = 0 + 96;
+            /// <summary>GPIO pin.</summary>
+            public const int PD1 = 1 + 96;
+            /// <summary>GPIO pin.</summary>
+            public const int PD2 = 2 + 96;
+            /// <summary>GPIO pin.</summary>
+            public const int PD3 = 3 + 96;
+            /// <summary>GPIO pin.</summary>
+            public const int PD4 = 4 + 96;
+            /// <summary>GPIO pin.</summary>
+            public const int PD5 = 5 + 96;
+            /// <summary>GPIO pin.</summary>
+            public const int PD6 = 6 + 96;
+            /// <summary>GPIO pin.</summary>
+            public const int PD7 = 7 + 96;
+            /// <summary>GPIO pin.</summary>
+            public const int PD8 = 8 + 96;
+            /// <summary>GPIO pin.</summary>
+            public const int PD9 = 9 + 96;
+            /// <summary>GPIO pin.</summary>
+            public const int PD10 = 10 + 96;
+            /// <summary>GPIO pin.</summary>
+            public const int PD11 = 11 + 96;
+            /// <summary>GPIO pin.</summary>
+            public const int PD12 = 12 + 96;
+            /// <summary>GPIO pin.</summary>
+            public const int PD13 = 13 + 96;
+            /// <summary>GPIO pin.</summary>
+            public const int PD14 = 14 + 96;
+            /// <summary>GPIO pin.</summary>
+            public const int PD15 = 15 + 96;
+            /// <summary>GPIO pin.</summary>
+            public const int PD16 = 16 + 96;
+            /// <summary>GPIO pin.</summary>
+            public const int PD17 = 17 + 96;
+            /// <summary>GPIO pin.</summary>
+            public const int PD18 = 18 + 96;
+        }
+
+        /// <summary>ADC channel definitions.</summary>
+        public static class AdcChannel {
+            /// <summary>ADC channel.</summary>
+            public const int PB11 = 0;
+            /// <summary>ADC channel.</summary>
+            public const int PB12 = 1;
+            /// <summary>ADC channel.</summary>
+            public const int PB13 = 2;
+            /// <summary>ADC channel.</summary>
+            public const int PB14 = 3;
+            /// <summary>ADC channel.</summary>
+            public const int PB15 = 4;
+            /// <summary>ADC channel.</summary>
+            public const int PB16 = 5;
+            /// <summary>ADC channel.</summary>
+            public const int PB17 = 6;
+            /// <summary>ADC channel.</summary>
+            public const int PB8 = 9;
+        }
+
+        /// <summary>PWM pin definitions.</summary>
+        public static class PwmPin {
+            /// <summary>PWM controller.</summary>
+            public static class Controller0 {
+                public const string Id = "CON0";
+
+                /// <summary>PWM pin.</summary>
+                public const int PC18 = 0;
+            }
+
+            /// <summary>PWM controller.</summary>
+            public static class Controller1 {
+                public const string Id = "CON1";
+
+                /// <summary>PWM pin.</summary>
+                public const int PC19 = 1;
+            }
+
+            /// <summary>PWM controller.</summary>
+            public static class Controller2 {
+                public const string Id = "CON2";
+
+                /// <summary>PWM pin.</summary>
+                public const int PC20 = 2;
+            }
+
+            /// <summary>PWM controller.</summary>
+            public static class Controller3 {
+                public const string Id = "CON3";
+
+                /// <summary>PWM pin.</summary>
+                public const int PC21 = 3;
+            }
+        }
+
+        /// <summary>Serial port definitions.</summary>
+        public static class SerialPort {
+            /// <summary>Serial port on PA10 (TX) and PA9 (RX).</summary>
+            public const string Com1 = "COM1";
+            /// <summary>Serial port on PA0 (TX), PA1 (RX), PA3 (CTS), and PA2 (RTS).</summary>
+            public const string Com2 = "COM2";
+            /// <summary>Serial port on PA5 (TX) and PA6 (RX).</summary>
+            public const string Com3 = "COM3";
+            /// <summary>Serial port on PA7 (TX) and PA8 (RX).</summary>
+            public const string Com4 = "COM4";
+            /// <summary>Serial port on PC8 (TX) and PC9 (RX).</summary>
+            public const string Com5 = "COM5";
+            /// <summary>Serial port on PC16 (TX) and PC17 (RX).</summary>
+            public const string Com6 = "COM6";
+        }
+
+        /// <summary>I2C bus definitions.</summary>
+        public static class I2cBus {
+            /// <summary>I2C bus on PA30 (SDA) and PA31 (SCL).</summary>
+            public const string I2c1 = "I2C1";
+        }
+
+        /// <summary>SPI bus definitions.</summary>
+        public static class SpiBus {
+            /// <summary>SPI bus.</summary>
+            public const string Spi1 = "SPI1";
+            /// <summary>SPI bus on PA22 (MOSI), PA21 (MISO), and PA23 (SCK).</summary>
+            public const string Spi2 = "SPI2";
+        }
+
+        /// <summary>CAN bus definitions.</summary>
+        public static class CanBus {
+            /// <summary>CAN channel on PA10 (TX) and PA9 (RX).</summary>
+            public const string Can1 = "CAN1";
+            /// <summary>CAN channel on PA5 (TX) and PA6 (RX).</summary>
+            public const string Can2 = "CAN2";
+        }
+    }
+}
