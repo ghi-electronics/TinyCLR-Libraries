6cee3b1353921138ec2499abea4664cf9ef5034e
John Brochue <john.brochue@ghielectronics.com>
2017-03-23 12:01:25 -0400

    Added GpioProvider.

--- GHIElectronics.TinyCLR.Devices/GpioController.cs
+++ GHIElectronics.TinyCLR.Devices/GpioController.cs
@@ -1,12 +1,14 @@
-using System;
-using System.Runtime.CompilerServices;
+using GHIElectronics.TinyCLR.Devices.Gpio.Provider;
 
 namespace GHIElectronics.TinyCLR.Devices.Gpio {
     /// <summary>
     /// Represents the default general-purpose I/O (GPIO) controller for the system.
     /// </summary>
     public sealed class GpioController {
-        private static GpioController s_instance = new GpioController();
+        private readonly IGpioControllerProvider provider;
+        private static GpioController instance;
+
+        internal GpioController(IGpioControllerProvider provider) => this.provider = provider;
 
         /// <summary>
         /// Gets the number of pins on the general-purpose I/O (GPIO) controller.
@@ -14,16 +16,23 @@ namespace GHIElectronics.TinyCLR.Devices.Gpio {
         /// <value>The number of pins on the GPIO controller. Some pins may not be available in user mode. For
         ///     information about how the pin numbers correspond to physical pins, see the documentation for your
         ///     circuit board.</value>
-        extern public int PinCount {
-            [MethodImplAttribute(MethodImplOptions.InternalCall)]
-            get;
-        }
+        public int PinCount => this.provider.PinCount;
 
         /// <summary>
         /// Gets the default general-purpose I/O (GPIO) controller for the system.
         /// </summary>
         /// <returns>The default GPIO controller for the system, or null if the system has no GPIO controller.</returns>
-        public static GpioController GetDefault() => s_instance;
+        public static GpioController GetDefault() => GpioController.instance ?? (GpioController.instance = new GpioController(new DefaultGpioControllerProvider()));
+
+        public static GpioController[] GetControllers(IGpioProvider provider) {
+            var providers = provider.GetControllers();
+            var controllers = new GpioController[providers.Length];
+
+            for (var i = 0; i < providers.Length; i++)
+                controllers[i] = new GpioController(providers[i]);
+
+            return controllers;
+        }
 
         /// <summary>
         /// Opens a connection to the specified general-purpose I/O (GPIO) pin in exclusive mode.
@@ -43,14 +52,7 @@ namespace GHIElectronics.TinyCLR.Devices.Gpio {
         /// <param name="sharingMode">The mode in which you want to open the GPIO pin, which determines whether other
         ///     connections to the pin can be opened while you have the pin open.</param>
         /// <returns>The opened GPIO pin.</returns>
-        public GpioPin OpenPin(int pinNumber, GpioSharingMode sharingMode) {
-            var pin = new GpioPin();
-            if (!pin.Init(pinNumber)) {
-                throw new InvalidOperationException();
-            }
-
-            return pin;
-        }
+        public GpioPin OpenPin(int pinNumber, GpioSharingMode sharingMode) => new GpioPin(this.provider.OpenPinProvider(pinNumber, (ProviderGpioSharingMode)sharingMode));
 
         /// <summary>
         /// Opens the specified general-purpose I/O (GPIO) pin in the specified mode, and gets a status value that can
@@ -66,16 +68,16 @@ namespace GHIElectronics.TinyCLR.Devices.Gpio {
         ///     succeeded, or the reason that the attempt to open the GPIO pin failed.</param>
         /// <returns>True if the pin could be opened; otherwise false.</returns>
         public bool TryOpenPin(int pinNumber, GpioSharingMode sharingMode, out GpioPin pin, out GpioOpenStatus openStatus) {
-            var newPin = new GpioPin();
-            if (!newPin.Init(pinNumber)) {
-                pin = null;
-                openStatus = GpioOpenStatus.PinUnavailable;
+            try {
+                pin = new GpioPin(this.provider.OpenPinProvider(pinNumber, (ProviderGpioSharingMode)sharingMode));
+                openStatus = GpioOpenStatus.PinOpened;
                 return true;
             }
-
-            pin = newPin;
-            openStatus = GpioOpenStatus.PinOpened;
-            return true;
+            catch {
+                pin = default(GpioPin);
+                openStatus = GpioOpenStatus.PinUnavailable;
+                return false;
+            }
         }
     }
 }
