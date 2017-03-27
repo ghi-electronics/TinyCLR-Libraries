cfb0c4521d374317f24888dc92bb8cb76f0a76cf
John Brochue <john.brochue@ghielectronics.com>
2017-03-27 14:56:14 -0400

    Added graphics library with display configuration.

---
+++ GHIElectronics.TinyCLR.Graphics/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GHIElectronics.TinyCLR.Graphics")]
+[assembly: AssemblyDescription("TinyCLR OS graphics library.")]
+[assembly: AssemblyCompany("GHI Electronics, LLC")]
+[assembly: AssemblyProduct("TinyCLR OS")]
+[assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2017")]
+[assembly: ComVisible(false)]
+[assembly: Guid("52A54E94-2FD8-4EEE-822C-C8C7F27AF423")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
