d5cd181572af72bed14b5e8485031e34a1947d33
John Brochue <john.brochue@ghielectronics.com>
2017-03-28 13:11:58 -0400

    Filled out IntPtr and UIntPtr.

--- GHIElectronics.TinyCLR.Core/IntPtr.cs
+++ GHIElectronics.TinyCLR.Core/IntPtr.cs
@@ -1,7 +1,40 @@
+using System.Globalization;
+using System.Runtime.InteropServices;
+
 namespace System {
     [Serializable]
+    [ComVisible(true)]
     public struct IntPtr {
+        private int value;
+
+        public static readonly IntPtr Zero;
+
+        public IntPtr(int value) => this.value = value;
+        public IntPtr(long value) => this.value = checked((int)value);
+
+        public override bool Equals(object obj) => obj is IntPtr ptr && this.value == ptr.value;
+        public override int GetHashCode() => this.value;
+
+        public int ToInt32() => this.value;
+        public long ToInt64() => this.value;
+
+        public override string ToString() => this.ToString(null);
+        public string ToString(string format) => this.value.ToString(format, CultureInfo.InvariantCulture);
+
+        public static explicit operator IntPtr(int value) => new IntPtr(value);
+        public static explicit operator IntPtr(long value) => new IntPtr(value);
+
+        public static explicit operator int(IntPtr value) => value.value;
+        public static explicit operator long(IntPtr value) => value.value;
+
+        public static bool operator ==(IntPtr value1, IntPtr value2) => value1.value == value2.value;
+        public static bool operator !=(IntPtr value1, IntPtr value2) => value1.value != value2.value;
+        public static IntPtr operator +(IntPtr pointer, int offset) => new IntPtr(pointer.ToInt32() + offset);
+        public static IntPtr operator -(IntPtr pointer, int offset) => new IntPtr(pointer.ToInt32() - offset);
+
+        public static IntPtr Add(IntPtr pointer, int offset) => pointer + offset;
+        public static IntPtr Subtract(IntPtr pointer, int offset) => pointer - offset;
+
+        public static int Size => 4;
     }
 }
-
-
--- GHIElectronics.TinyCLR.Core/UIntPtr.cs
+++ GHIElectronics.TinyCLR.Core/UIntPtr.cs
@@ -1,9 +1,41 @@
-namespace System {
+using System.Globalization;
+using System.Runtime.InteropServices;
 
+namespace System {
     [Serializable]
-    [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
+    [CLSCompliant(false)]
+    [ComVisible(true)]
     public struct UIntPtr {
+        private uint value;
+
+        public static readonly UIntPtr Zero;
+
+        public UIntPtr(uint value) => this.value = value;
+        public UIntPtr(ulong value) => this.value = checked((uint)value);
+
+        public override bool Equals(object obj) => obj is UIntPtr ptr && this.value == ptr.value;
+        public override int GetHashCode() => unchecked((int)this.value) & 0x7FFFFFFF;
+
+        public uint ToUInt32() => this.value;
+        public ulong ToUInt64() => this.value;
+
+        public override string ToString() => this.ToString(null);
+        public string ToString(string format) => this.value.ToString(format, CultureInfo.InvariantCulture);
+
+        public static explicit operator UIntPtr(uint value) => new UIntPtr(value);
+        public static explicit operator UIntPtr(ulong value) => new UIntPtr(value);
+
+        public static explicit operator uint(UIntPtr value) => value.value;
+        public static explicit operator ulong(UIntPtr value) => value.value;
+
+        public static bool operator ==(UIntPtr value1, UIntPtr value2) => value1.value == value2.value;
+        public static bool operator !=(UIntPtr value1, UIntPtr value2) => value1.value != value2.value;
+        public static UIntPtr operator +(UIntPtr pointer, int offset) => new UIntPtr(pointer.ToUInt32() + (uint)offset);
+        public static UIntPtr operator -(UIntPtr pointer, int offset) => new UIntPtr(pointer.ToUInt32() - (uint)offset);
+
+        public static UIntPtr Add(UIntPtr pointer, int offset) => pointer + offset;
+        public static UIntPtr Subtract(UIntPtr pointer, int offset) => pointer - offset;
+
+        public static int Size => 4;
     }
 }
-
-
