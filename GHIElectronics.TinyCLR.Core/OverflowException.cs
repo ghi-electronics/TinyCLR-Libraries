bba9a90c3e1ab8777dd30dafe1cc6c80e9f42bdc
John Brochue <john.brochue@ghielectronics.com>
2017-03-28 15:58:03 -0400

    Added OverflowException.

---
+++ GHIElectronics.TinyCLR.Core/OverflowException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace System {
+    [ComVisible(true)]
+    [Serializable]
+    public class OverflowException : Exception {
+        public OverflowException() : base("Overflow") { }
+        public OverflowException(string message) : base(message) { }
+        public OverflowException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
