6fd0f2506e8490d4e712fe9e4d481b2dd44ba9cc
John Brochue <john.brochue@ghielectronics.com>
2017-03-31 10:13:54 -0400

    Added basic BrainPad driver.

---
+++ GHIElectronics.TinyCLR.BrainPad/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GHIElectronics.TinyCLR.Pins")]
+[assembly: AssemblyDescription("TinyCLR OS BrainPad library.")]
+[assembly: AssemblyCompany("GHI Electronics, LLC")]
+[assembly: AssemblyProduct("BrainPad")]
+[assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2017")]
+[assembly: ComVisible(false)]
+[assembly: Guid("50B4B973-7625-44B2-8304-369790A68B29")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
