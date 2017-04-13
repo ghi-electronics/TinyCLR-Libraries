9db2682b53e1b063426905a420eadf5c7e077c7b
John Brochue <john.brochue@ghielectronics.com>
2017-04-13 13:47:31 -0400

    Added EventArgs class.

---
+++ GHIElectronics.TinyCLR.Core/EventArgs.cs
@@ -0,0 +1,9 @@
+﻿using System.Runtime.InteropServices;
+
+namespace System {
+    [Serializable]
+    [ComVisible(true)]
+    public class EventArgs {
+        public static readonly EventArgs Empty = new EventArgs();
+    }
+}
