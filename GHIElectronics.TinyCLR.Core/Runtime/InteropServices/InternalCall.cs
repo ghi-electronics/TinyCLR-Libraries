0ce0f5569f15ddbb0efe8d278da13b96e94f10c2
John Brochue <john.brochue@ghielectronics.com>
2017-05-02 17:22:44 -0400

    Added a method to get the runtime address.

--- GHIElectronics.TinyCLR.Core/Runtime/InteropServices/InternalCall.cs
+++ GHIElectronics.TinyCLR.Core/Runtime/InteropServices/InternalCall.cs
@@ -4,5 +4,7 @@ namespace System.Runtime.InteropServices {
     public static class InternalCall {
         [MethodImpl(MethodImplOptions.InternalCall)]
         public static extern void Register(IntPtr address);
+
+        public static extern IntPtr RuntimeAddress { [MethodImpl(MethodImplOptions.InternalCall)] get; }
     }
 }
