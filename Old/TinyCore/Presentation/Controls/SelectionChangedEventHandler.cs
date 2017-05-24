d5130aaa5ebc199870df3671148a5a8b0029f996
John Brochue <john.brochue@ghielectronics.com>
2017-05-24 17:53:29 -0400

    Removed old libraries.

--- Old/TinyCore/Presentation/Controls/SelectionChangedEventHandler.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using Microsoft.SPOT.Presentation.Media;
-
-namespace Microsoft.SPOT.Presentation.Controls
-{
-    public delegate void SelectionChangedEventHandler(object sender, SelectionChangedEventArgs args);
-}
-
-
