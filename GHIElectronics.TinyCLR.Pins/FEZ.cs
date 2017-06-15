de3c7dde2fbc79b7ddfacba2899dac07d6ddbea5
John Brochue <john.brochue@ghielectronics.com>
2017-06-15 16:24:09 -0400

    Updated FEZ pins.

--- GHIElectronics.TinyCLR.Pins/FEZ.cs
+++ GHIElectronics.TinyCLR.Pins/FEZ.cs
@@ -5,102 +5,58 @@
         public static class GpioPin {
             public const string Id = "GHIElectronics.TinyCLR.NativeApis.FEZ.GpioProvider\\0";
 
+            /// <summary>GPIO pin for LED1.</summary>
+            public const int Led1 = G30.GpioPin.PB9;
+            /// <summary>GPIO pin for LED2.</summary>
+            public const int Led2 = G30.GpioPin.PB8;
+            /// <summary>GPIO pin for BTN1.</summary>
+            public const int Btn1 = G30.GpioPin.PC13;
+            /// <summary>GPIO pin for BTN2.</summary>
+            public const int Btn2 = G30.GpioPin.PA15;
             /// <summary>GPIO pin.</summary>
-            public const int PA0 = 0;
+            public const int A0 = G30.GpioPin.PA4;
             /// <summary>GPIO pin.</summary>
-            public const int PA1 = 1;
+            public const int A1 = G30.GpioPin.PA5;
             /// <summary>GPIO pin.</summary>
-            public const int PA2 = 2;
+            public const int A2 = G30.GpioPin.PA6;
             /// <summary>GPIO pin.</summary>
-            public const int PA3 = 3;
+            public const int A3 = G30.GpioPin.PA7;
             /// <summary>GPIO pin.</summary>
-            public const int PA4 = 4;
+            public const int A4 = G30.GpioPin.PB0;
             /// <summary>GPIO pin.</summary>
-            public const int PA5 = 5;
+            public const int A5 = G30.GpioPin.PB1;
             /// <summary>GPIO pin.</summary>
-            public const int PA6 = 6;
+            public const int D0 = G30.GpioPin.PA10;
             /// <summary>GPIO pin.</summary>
-            public const int PA7 = 7;
+            public const int D1 = G30.GpioPin.PA9;
             /// <summary>GPIO pin.</summary>
-            public const int PA8 = 8;
+            public const int D2 = G30.GpioPin.PC1;
             /// <summary>GPIO pin.</summary>
-            public const int PA9 = 9;
+            public const int D3 = G30.GpioPin.PA8;
             /// <summary>GPIO pin.</summary>
-            public const int PA10 = 10;
+            public const int D4 = G30.GpioPin.PC15;
             /// <summary>GPIO pin.</summary>
-            public const int PA11 = 11;
+            public const int D5 = G30.GpioPin.PA1;
             /// <summary>GPIO pin.</summary>
-            public const int PA12 = 12;
+            public const int D6 = G30.GpioPin.PA0;
             /// <summary>GPIO pin.</summary>
-            public const int PA13 = 13;
+            public const int D7 = G30.GpioPin.PC14;
             /// <summary>GPIO pin.</summary>
-            public const int PA14 = 14;
+            public const int D8 = G30.GpioPin.PC0;
             /// <summary>GPIO pin.</summary>
-            public const int PA15 = 15;
+            public const int D9 = G30.GpioPin.PC9;
             /// <summary>GPIO pin.</summary>
-            public const int PB0 = 0 + 16;
+            public const int D10 = G30.GpioPin.PC8;
             /// <summary>GPIO pin.</summary>
-            public const int PB1 = 1 + 16;
+            public const int D11 = G30.GpioPin.PB5;
             /// <summary>GPIO pin.</summary>
-            public const int PB2 = 2 + 16;
+            public const int D12 = G30.GpioPin.PB4;
             /// <summary>GPIO pin.</summary>
-            public const int PB3 = 3 + 16;
+            public const int D13 = G30.GpioPin.PB3;
             /// <summary>GPIO pin.</summary>
-            public const int PB4 = 4 + 16;
+            public const int Sda = G30.GpioPin.PB7;
             /// <summary>GPIO pin.</summary>
-            public const int PB5 = 5 + 16;
-            /// <summary>GPIO pin.</summary>
-            public const int PB6 = 6 + 16;
-            /// <summary>GPIO pin.</summary>
-            public const int PB7 = 7 + 16;
-            /// <summary>GPIO pin.</summary>
-            public const int PB8 = 8 + 16;
-            /// <summary>GPIO pin.</summary>
-            public const int PB9 = 9 + 16;
-            /// <summary>GPIO pin.</summary>
-            public const int PB10 = 10 + 16;
-            /// <summary>GPIO pin.</summary>
-            public const int PB12 = 12 + 16;
-            /// <summary>GPIO pin.</summary>
-            public const int PB13 = 13 + 16;
-            /// <summary>GPIO pin.</summary>
-            public const int PB14 = 14 + 16;
-            /// <summary>GPIO pin.</summary>
-            public const int PB15 = 15 + 16;
-            /// <summary>GPIO pin.</summary>
-            public const int PC0 = 0 + 32;
-            /// <summary>GPIO pin.</summary>
-            public const int PC1 = 1 + 32;
-            /// <summary>GPIO pin.</summary>
-            public const int PC2 = 2 + 32;
-            /// <summary>GPIO pin.</summary>
-            public const int PC3 = 3 + 32;
-            /// <summary>GPIO pin.</summary>
-            public const int PC4 = 4 + 32;
-            /// <summary>GPIO pin.</summary>
-            public const int PC5 = 5 + 32;
-            /// <summary>GPIO pin.</summary>
-            public const int PC6 = 6 + 32;
-            /// <summary>GPIO pin.</summary>
-            public const int PC7 = 7 + 32;
-            /// <summary>GPIO pin.</summary>
-            public const int PC8 = 8 + 32;
-            /// <summary>GPIO pin.</summary>
-            public const int PC9 = 9 + 32;
-            /// <summary>GPIO pin.</summary>
-            public const int PC10 = 10 + 32;
-            /// <summary>GPIO pin.</summary>
-            public const int PC11 = 11 + 32;
-            /// <summary>GPIO pin.</summary>
-            public const int PC12 = 12 + 32;
-            /// <summary>GPIO pin.</summary>
-            public const int PC13 = 13 + 32;
-            /// <summary>GPIO pin.</summary>
-            public const int PC14 = 14 + 32;
-            /// <summary>GPIO pin.</summary>
-            public const int PC15 = 15 + 32;
-            /// <summary>GPIO pin.</summary>
-            public const int PD2 = 2 + 48;
+            public const int Scl = G30.GpioPin.PB6;
         }
 
         /// <summary>ADC channel definitions.</summary>
@@ -108,37 +64,25 @@
             public const string Id = "GHIElectronics.TinyCLR.NativeApis.FEZ.AdcProvider\\0";
 
             /// <summary>ADC channel.</summary>
-            public const int PA0 = 0;
+            public const int A0 = G30.AdcChannel.PA4;
             /// <summary>ADC channel.</summary>
-            public const int PA1 = 1;
+            public const int A1 = G30.AdcChannel.PA5;
             /// <summary>ADC channel.</summary>
-            public const int PA2 = 2;
+            public const int A2 = G30.AdcChannel.PA6;
             /// <summary>ADC channel.</summary>
-            public const int PA3 = 3;
+            public const int A3 = G30.AdcChannel.PA7;
             /// <summary>ADC channel.</summary>
-            public const int PA4 = 4;
+            public const int A4 = G30.AdcChannel.PB0;
             /// <summary>ADC channel.</summary>
-            public const int PA5 = 5;
+            public const int A5 = G30.AdcChannel.PB1;
             /// <summary>ADC channel.</summary>
-            public const int PA6 = 6;
+            public const int D2 = G30.AdcChannel.PC1;
             /// <summary>ADC channel.</summary>
-            public const int PA7 = 7;
+            public const int D5 = G30.AdcChannel.PA1;
             /// <summary>ADC channel.</summary>
-            public const int PB0 = 8;
+            public const int D6 = G30.AdcChannel.PA0;
             /// <summary>ADC channel.</summary>
-            public const int PB1 = 9;
-            /// <summary>ADC channel.</summary>
-            public const int PC0 = 10;
-            /// <summary>ADC channel.</summary>
-            public const int PC1 = 11;
-            /// <summary>ADC channel.</summary>
-            public const int PC2 = 12;
-            /// <summary>ADC channel.</summary>
-            public const int PC3 = 13;
-            /// <summary>ADC channel.</summary>
-            public const int PC4 = 14;
-            /// <summary>ADC channel.</summary>
-            public const int PC5 = 15;
+            public const int D8 = G30.AdcChannel.PC0;
         }
 
         /// <summary>PWM pin definitions.</summary>
@@ -148,11 +92,11 @@
                 public const string Id = "GHIElectronics.TinyCLR.NativeApis.FEZ.PwmProvider\\0";
 
                 /// <summary>PWM pin.</summary>
-                public const int PA8 = 0;
+                public const int D3 = 0;
                 /// <summary>PWM pin.</summary>
-                public const int PA9 = 1;
+                public const int D1 = 1;
                 /// <summary>PWM pin.</summary>
-                public const int PA10 = 2;
+                public const int D0 = 2;
             }
 
             /// <summary>PWM controller.</summary>
@@ -160,13 +104,9 @@
                 public const string Id = "GHIElectronics.TinyCLR.NativeApis.FEZ.PwmProvider\\1";
 
                 /// <summary>PWM pin.</summary>
-                public const int PA0 = 0;
+                public const int D6 = 0;
                 /// <summary>PWM pin.</summary>
-                public const int PA1 = 1;
-                /// <summary>PWM pin.</summary>
-                public const int PA2 = 2;
-                /// <summary>PWM pin.</summary>
-                public const int PA3 = 3;
+                public const int D5 = 1;
             }
 
             /// <summary>PWM controller.</summary>
@@ -174,13 +114,11 @@
                 public const string Id = "GHIElectronics.TinyCLR.NativeApis.FEZ.PwmProvider\\2";
 
                 /// <summary>PWM pin.</summary>
-                public const int PC6 = 0;
+                public const int D11 = 1;
                 /// <summary>PWM pin.</summary>
-                public const int PB5 = 1;
+                public const int D10 = 2;
                 /// <summary>PWM pin.</summary>
-                public const int PC8 = 2;
-                /// <summary>PWM pin.</summary>
-                public const int PC9 = 3;
+                public const int D9 = 3;
             }
 
             /// <summary>PWM controller.</summary>
@@ -188,36 +126,28 @@
                 public const string Id = "GHIElectronics.TinyCLR.NativeApis.FEZ.PwmProvider\\3";
 
                 /// <summary>PWM pin.</summary>
-                public const int PB6 = 0;
+                public const int Scl = 0;
                 /// <summary>PWM pin.</summary>
-                public const int PB7 = 1;
-                /// <summary>PWM pin.</summary>
-                public const int PB8 = 2;
-                /// <summary>PWM pin.</summary>
-                public const int PB9 = 3;
+                public const int Sda = 1;
             }
         }
 
         /// <summary>Serial port definitions.</summary>
         public static class SerialPort {
-            /// <summary>Serial port on PA9 (TX) and PA10 (RX).</summary>
+            /// <summary>Serial port on D1 (TX) and D0 (RX).</summary>
             public const string Com1 = "GHIElectronics.TinyCLR.NativeApis.FEZ.UartProvider\\0";
-            /// <summary>Serial port on PA2 (TX), PA3 (RX), PA0 (CTS), and PA1 (RTS).</summary>
-            public const string Com2 = "GHIElectronics.TinyCLR.NativeApis.FEZ.UartProvider\\1";
         }
 
         /// <summary>I2C bus definitions.</summary>
         public static class I2cBus {
-            /// <summary>I2C bus on PB7 (SDA) and PB6 (SCL).</summary>
+            /// <summary>I2C bus on SDA (SDA) and SCL (SCL).</summary>
             public const string I2c1 = "GHIElectronics.TinyCLR.NativeApis.FEZ.I2cProvider\\0";
         }
 
         /// <summary>SPI bus definitions.</summary>
         public static class SpiBus {
-            /// <summary>SPI bus on PB5 (MOSI), PB4 (MISO), and PB3 (SCK).</summary>
+            /// <summary>SPI bus on D11 (MOSI), D12 (MISO), and D13 (SCK).</summary>
             public const string Spi1 = "GHIElectronics.TinyCLR.NativeApis.FEZ.SpiProvider\\0";
-            /// <summary>SPI bus on PB15 (MOSI), PB14 (MISO), and PB13 (SCK).</summary>
-            public const string Spi2 = "GHIElectronics.TinyCLR.NativeApis.FEZ.SpiProvider\\1";
         }
     }
 }
