df47a3831bd8cbfda6c6266842eb24abff441d4d
John Brochue <john.brochue@ghielectronics.com>
2017-07-05 13:44:54 -0400

    Added class for getting device infomation, fixes #115.

---
+++ mscorlib/Runtime/InteropServices/DeviceInformation.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace System.Runtime.InteropServices {
+    public static class DeviceInformation {
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        static extern DeviceInformation();
+
+        public static string DeviceName { get; }
+        public static string ManufacturerName { get; }
+        [CLSCompliant(false)]
+        public static ulong Version { get; }
+    }
+}
