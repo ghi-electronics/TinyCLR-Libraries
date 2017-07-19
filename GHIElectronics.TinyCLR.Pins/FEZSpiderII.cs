c880961da61a585949b41b76fbdae77b08690921
John Brochue <john.brochue@ghielectronics.com>
2017-07-19 14:26:22 -0400

    Added FEZ Spider II to pins, #122

---
+++ GHIElectronics.TinyCLR.Pins/FEZSpiderII.cs
@@ -0,0 +1,191 @@
+﻿namespace GHIElectronics.TinyCLR.Pins {
+    public static class FEZSpiderII {
+        public static class GpioPin {
+            public const int DebugLed = G120E.GpioPin.P1_31;
+
+            public static class Socket1 {
+                public const int Pin3 = G120E.GpioPin.P2_26;
+                public const int Pin6 = G120E.GpioPin.P2_22;
+                public const int Pin7 = G120E.GpioPin.P4_31;
+                public const int Pin8 = G120E.GpioPin.P0_27;
+                public const int Pin9 = G120E.GpioPin.P0_28;
+            }
+
+            public static class Socket3 {
+                public const int Pin3 = G120E.GpioPin.P0_5;
+                public const int Pin6 = G120E.GpioPin.P0_4;
+                public const int Pin8 = G120E.GpioPin.P0_27;
+                public const int Pin9 = G120E.GpioPin.P0_28;
+            }
+
+            public static class Socket4 {
+                public const int Pin3 = G120E.GpioPin.P2_31;
+                public const int Pin4 = G120E.GpioPin.P2_0;
+                public const int Pin5 = G120E.GpioPin.P0_16;
+                public const int Pin6 = G120E.GpioPin.P0_6;
+                public const int Pin7 = G120E.GpioPin.P3_18;
+                public const int Pin8 = G120E.GpioPin.P0_27;
+                public const int Pin9 = G120E.GpioPin.P0_28;
+            }
+
+            public static class Socket5 {
+                public const int Pin3 = G120E.GpioPin.P2_27;
+                public const int Pin4 = G120E.GpioPin.P1_6;
+                public const int Pin5 = G120E.GpioPin.P1_7;
+                public const int Pin6 = G120E.GpioPin.P1_3;
+                public const int Pin7 = G120E.GpioPin.P1_11;
+                public const int Pin8 = G120E.GpioPin.P1_12;
+                public const int Pin9 = G120E.GpioPin.P1_2;
+            }
+
+            public static class Socket6 {
+                public const int Pin3 = G120E.GpioPin.P2_25;
+                public const int Pin4 = G120E.GpioPin.P0_1;
+                public const int Pin5 = G120E.GpioPin.P0_0;
+                public const int Pin6 = G120E.GpioPin.P4_28;
+            }
+
+            public static class Socket8 {
+                public const int Pin3 = G120E.GpioPin.P2_10;
+                public const int Pin4 = G120E.GpioPin.P0_10;
+                public const int Pin5 = G120E.GpioPin.P0_11;
+                public const int Pin6 = G120E.GpioPin.P1_19;
+                public const int Pin7 = G120E.GpioPin.P3_29;
+                public const int Pin8 = G120E.GpioPin.P3_27;
+                public const int Pin9 = G120E.GpioPin.P3_26;
+            }
+
+            public static class Socket9 {
+                public const int Pin3 = G120E.GpioPin.P0_13;
+                public const int Pin4 = G120E.GpioPin.P0_25;
+                public const int Pin5 = G120E.GpioPin.P0_26;
+                public const int Pin6 = G120E.GpioPin.P3_25;
+                public const int Pin7 = G120E.GpioPin.P0_18;
+                public const int Pin8 = G120E.GpioPin.P0_17;
+                public const int Pin9 = G120E.GpioPin.P0_15;
+            }
+
+            public static class Socket10 {
+                public const int Pin3 = G120E.GpioPin.P0_12;
+                public const int Pin4 = G120E.GpioPin.P0_24;
+                public const int Pin5 = G120E.GpioPin.P0_23;
+                public const int Pin6 = G120E.GpioPin.P3_31;
+                public const int Pin7 = G120E.GpioPin.P2_23;
+                public const int Pin8 = G120E.GpioPin.P0_27;
+                public const int Pin9 = G120E.GpioPin.P0_28;
+            }
+
+            public static class Socket11 {
+                public const int Pin3 = G120E.GpioPin.P2_30;
+                public const int Pin4 = G120E.GpioPin.P0_2;
+                public const int Pin5 = G120E.GpioPin.P0_3;
+                public const int Pin6 = G120E.GpioPin.P4_29;
+                public const int Pin7 = G120E.GpioPin.P3_24;
+                public const int Pin8 = G120E.GpioPin.P3_16;
+                public const int Pin9 = G120E.GpioPin.P3_17;
+            }
+
+            public static class Socket12 {
+                public const int Pin3 = G120E.GpioPin.P2_13;
+                public const int Pin4 = G120E.GpioPin.P1_26;
+                public const int Pin5 = G120E.GpioPin.P1_27;
+                public const int Pin6 = G120E.GpioPin.P1_28;
+                public const int Pin7 = G120E.GpioPin.P1_29;
+                public const int Pin8 = G120E.GpioPin.P2_4;
+                public const int Pin9 = G120E.GpioPin.P2_2;
+            }
+
+            public static class Socket13 {
+                public const int Pin3 = G120E.GpioPin.P1_20;
+                public const int Pin4 = G120E.GpioPin.P1_21;
+                public const int Pin5 = G120E.GpioPin.P1_22;
+                public const int Pin6 = G120E.GpioPin.P1_23;
+                public const int Pin7 = G120E.GpioPin.P1_24;
+                public const int Pin8 = G120E.GpioPin.P1_25;
+                public const int Pin9 = G120E.GpioPin.P2_21;
+            }
+
+            public static class Socket14 {
+                public const int Pin3 = G120E.GpioPin.P2_12;
+                public const int Pin4 = G120E.GpioPin.P2_6;
+                public const int Pin5 = G120E.GpioPin.P2_7;
+                public const int Pin6 = G120E.GpioPin.P2_8;
+                public const int Pin7 = G120E.GpioPin.P2_9;
+                public const int Pin8 = G120E.GpioPin.P2_3;
+                public const int Pin9 = G120E.GpioPin.P2_5;
+            }
+        }
+
+        public static class AdcChannel {
+            public static class Socket9 {
+                public const int Pin3 = G120E.AdcChannel.P0_13;
+                public const int Pin4 = G120E.AdcChannel.P0_25;
+                public const int Pin5 = G120E.AdcChannel.P0_26;
+            }
+
+            public static class Socket10 {
+                public const int Pin3 = G120E.AdcChannel.P0_12;
+                public const int Pin4 = G120E.AdcChannel.P0_24;
+                public const int Pin5 = G120E.AdcChannel.P0_23;
+            }
+        }
+
+        public static class DacChannel {
+            public static class Socket9 {
+                public const int Pin5 = G120E.DacChannel.P0_26;
+            }
+        }
+
+        public static class PwmPin {
+            public static class Controller1 {
+                public const string Id = G120E.PwmPin.Controller1.Id;
+
+                public static class Socket8 {
+                    public const int Pin7 = G120E.PwmPin.Controller1.P3_29;
+                    public const int Pin8 = G120E.PwmPin.Controller1.P3_27;
+                    public const int Pin9 = G120E.PwmPin.Controller1.P3_26;
+                }
+
+                public static class Socket11 {
+                    public const int Pin7 = G120E.PwmPin.Controller1.P3_24;
+                }
+            }
+
+            public static class Controller0 {
+                public const string Id = G120E.PwmPin.Controller0.Id;
+
+                public static class Socket11 {
+                    public const int Pin8 = G120E.PwmPin.Controller0.P3_16;
+                    public const int Pin9 = G120E.PwmPin.Controller0.P3_17;
+                }
+            }
+        }
+
+        public static class UartPort {
+            public const string Socket4 = G120E.UartPort.Uart1;
+            public const string Socket8 = G120E.UartPort.Uart2;
+            public const string Socket9 = G120E.UartPort.Uart3;
+            public const string Socket11 = G120E.UartPort.Uart0;
+        }
+
+        public static class I2cBus {
+            public const string Socket1 = G120E.I2cBus.I2c0;
+            public const string Socket3 = G120E.I2cBus.I2c0;
+            public const string Socket4 = G120E.I2cBus.I2c0;
+            public const string Socket10 = G120E.I2cBus.I2c0;
+        }
+
+        public static class SpiBus {
+            public const string Socket6 = G120E.SpiBus.Spi1;
+            public const string Socket9 = G120E.SpiBus.Spi0;
+        }
+
+        public static class CanBus {
+            public const string Socket6 = G120E.CanBus.Can1;
+        }
+
+        public static class Display {
+            public const string Lcd = G120E.Display.Lcd;
+        }
+    }
+}
