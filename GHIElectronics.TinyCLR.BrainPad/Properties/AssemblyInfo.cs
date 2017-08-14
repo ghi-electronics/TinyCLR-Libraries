ead980509a4869f714354e2ca40e6b7eebad9713
John Brochue <john.brochue@ghielectronics.com>
2017-08-14 15:14:01 -0400

    Updated versions.

--- GHIElectronics.TinyCLR.BrainPad/Properties/AssemblyInfo.cs
+++ GHIElectronics.TinyCLR.BrainPad/Properties/AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2017")]
 [assembly: ComVisible(false)]
 [assembly: Guid("50B4B973-7625-44B2-8304-369790A68B29")]
-[assembly: AssemblyVersion("0.5.0.0")]
-[assembly: AssemblyFileVersion("0.5.0.0")]
+[assembly: AssemblyVersion("0.6.0.0")]
+[assembly: AssemblyFileVersion("0.6.0.0")]
--- GHIElectronics.TinyCLR.Storage/Properties/AssemblyInfo.cs
+++ GHIElectronics.TinyCLR.Storage/Properties/AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2017")]
 [assembly: ComVisible(false)]
 [assembly: Guid("4EF770FD-F6AC-42F5-8C14-88C5C1B06832")]
-[assembly: AssemblyVersion("0.5.0.0")]
-[assembly: AssemblyFileVersion("0.5.0.0")]
+[assembly: AssemblyVersion("0.6.0.0")]
+[assembly: AssemblyFileVersion("0.6.0.0")]
