fe7cce59887b9ffbe7c518dd1e050d2b124090bc
Dat <dat.tran@ghielectronics.com>
2017-11-29 11:57:47 -0500

    Add Can Library

---
+++ GHIElectronics.TinyCLR.ControllerAreaNetwork/CAN.cs
@@ -0,0 +1,11 @@
+﻿namespace GHIElectronics.TinyCLR.ControllerAreaNetwork {
+    public class CAN {
+        public CAN(int controllerId, uint baudrate) {
+
+        }
+
+        public int Test() {
+            return 0x10;
+        }
+    }
+}
