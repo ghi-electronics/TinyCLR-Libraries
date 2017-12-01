b3b88b59c23febc06abee15fae268611688245ba
Dat <dat.tran@ghielectronics.com>
2017-12-01 15:24:13 -0500

    Raise Receiving and Error events in Managed code

--- GHIElectronics.TinyCLR.ControllerAreaNetwork/CanController.cs
+++ GHIElectronics.TinyCLR.ControllerAreaNetwork/CanController.cs
@@ -2,15 +2,23 @@
     public sealed class CanController {
         private ICanControllerProvider provider;
 
-        internal CanController(ICanControllerProvider provider) => this.provider = provider;
+        public delegate void MessageReceivedEventHandler(CanController sender, int count);
+        public delegate void ErrorReceivedEventHandler(CanController sender, Error error);
 
+        private CanEventListener canEventListener;
+
+        internal CanController(ICanControllerProvider provider) => this.provider = provider;
 
         public static CanController[] GetControllers(ICanProvider provider) {
             var providers = provider.GetControllers();
             var controllers = new CanController[providers.Length];
 
-            for (var i = 0; i < providers.Length; i++)
-                controllers[i] = new CanController(providers[i]);
+            for (var i = 0; i < providers.Length; i++) {
+                controllers[i] = new CanController(providers[i]) {
+                    canEventListener = new CanEventListener()
+                };
+                controllers[i].canEventListener.controller = controllers[i];
+            }
 
             return controllers;
         }
@@ -26,5 +34,12 @@
         public int SendMessages(Message[] messages, int offset, int count) => this.provider.SendMessages(messages, offset, count);
 
         public int ReceivedMessageCount() => this.provider.ReceivedMessageCount();
+
+        public event MessageReceivedEventHandler OnMessageReceived;
+        public event ErrorReceivedEventHandler OnErrorReceived;
+
+        internal void MessageReceived(int count) => OnMessageReceived?.Invoke(this, count);
+        internal void ErrorReceived(Error error) => OnErrorReceived?.Invoke(this, error);
+
     }
 }
---
+++ GHIElectronics.TinyCLR.ControllerAreaNetwork/CanEventListener.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace GHIElectronics.TinyCLR.ControllerAreaNetwork {
+    public class CanEventListener {
+        private NativeEventDispatcher dispatcherMessageReceived;
+        private NativeEventDispatcher dispatcherErrorReceived;
+
+        public CanController controller;
+
+        public CanEventListener() {
+            this.dispatcherMessageReceived = NativeEventDispatcher.GetDispatcher("GHIElectronics.TinyCLR.NativeEventNames.Can.MessageReceived");
+            this.dispatcherErrorReceived = NativeEventDispatcher.GetDispatcher("GHIElectronics.TinyCLR.NativeEventNames.Can.ErrorReceived");
+
+            this.dispatcherMessageReceived.OnInterrupt += this.Dispatcher_OnMessageReceivedInterrupt;
+            this.dispatcherErrorReceived.OnInterrupt += this.Dispatcher_OnErrorReceivedInterrupt;
+        }
+
+        private void Dispatcher_OnMessageReceivedInterrupt(string apiName, uint implementationIndex, ulong data0, ulong data1, IntPtr data2, DateTime timestamp) => this.controller.MessageReceived((int)data0);
+        private void Dispatcher_OnErrorReceivedInterrupt(string apiName, uint implementationIndex, ulong data0, ulong data1, IntPtr data2, DateTime timestamp) => this.controller.ErrorReceived((Error)data0);
+
+    }
+}
