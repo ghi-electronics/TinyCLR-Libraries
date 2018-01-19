50079bc756761eba441523c16ca630ef6c50b845
John Brochue <john.brochue@ghielectronics.com>
2018-01-19 12:11:33 -0500

    Removed more 01 from 04.

--- GHIElectronics.TinyCLR.Networking.SPWF04Sx/SPWF04SxInterface.cs
+++ GHIElectronics.TinyCLR.Networking.SPWF04Sx/SPWF04SxInterface.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections;
-using System.Diagnostics;
 using System.Text;
 using System.Threading;
 using GHIElectronics.TinyCLR.Devices.Gpio;
@@ -8,116 +7,19 @@ using GHIElectronics.TinyCLR.Devices.SerialCommunication;
 
 namespace GHIElectronics.TinyCLR.Networking.SPWF01Sx {
     public class SPWF04SxInterface {
+        public void Reset() => this.SendATCommand("AT+S.RESET");
 
-        private string socket;
-        private bool connected;
-        private bool error;
-        private int wait;
-        private string readline;
+        public void ConnectWiFi(string network, string password) {
+            this.StopWorker();
 
-        public object StringSplitOptions { get; private set; }
-
-        public enum SecurityMode {
-            none, // 0
-            WEP, // 1
-            WPA2Personal, // 2
-
-        }
-
-
-        public enum SocketType {
-            TCP, // t
-            UDP, // u
-            Secure, // s
-        }
-
-
-        public enum RadioMode {
-            IDLE,
-            STA,
-            IBSS,
-            MiniAP
-        }
-
-        public enum PasswordType {
-            OPEN,
-            WEP64,
-            WEP128,
-            WPA2,
-            WPA_TEXT
-        }
-
-
-
-        public class Command {
-            private Command() { }
-
-            public static readonly string Test = "AT";
-            public static readonly string Erase = "AT&F";
-            public static readonly string SetNetwork = "AT+S.SSIDTXT";
-            public static readonly string SetValue = "AT+S.SCFG=";
-            public static readonly string SaveConfig = "AT&W";
-            public static readonly string Status = "AT+S.STS";
-            public static readonly string Socket = "AT+S.SOCKD=";
-            public static readonly string HttpGet = "AT+S.HTTPGET=";
-            public static readonly string HttpPost = "AT+S.HTTPPOST=";
-            public static readonly string CloseSocket = "AT+S.SOCKC=";
-            public static readonly string ReadSocket = "AT+S.SOCKR=";
-            public static readonly string Ping = "AT+S.PING=";
-            public static readonly string SSID = "AT+S.SSIDTXT=";
-            public static readonly string AllowSSL = "AT+S.TLSCERT=";
-            public static readonly string TLSDOMAIN = "AT+S.TLSDOMAIN=";
-            public static readonly string SETTIME = "AT+S.SETTIME";
-            public static readonly string Clean = "AT+S.TLSCERT2=clean,all";
-            public static readonly string CertCA = "AT+S.TLSCERT=f_ca,";
-            public static readonly string CertClient = "AT+S.TLSCERT=f_cert,";
-            public static readonly string ClientKey = "AT+S.TLSCERT=f_key,";
-            public static readonly string OpenSocket = "AT+S.SOCKON=";
-            public static readonly string SetDomain = "AT+S.TLSDOMAIN=f_domain,";
-            public static readonly string CheckCerts = "AT+S.TLSCERT=f_content,0";
-            public static readonly string FWUpdate = "AT+S.HTTPDFSUPDATE=";
-            public static readonly string ScanNetworks = "AT+S.SCAN";
-            public static readonly string ServerSocket = "AT+S.SOCKD=";
-            public static readonly string Reset = "AT+CFUN=1";
-
-        }
-
-
-        public void ConnectWiFi(string network, string password, PasswordType passwordType, RadioMode radioMode, SecurityMode securityMode) {
-            string passtype, smode, radio;
-
-            switch (passwordType) {
-                case PasswordType.WEP64: passtype = "wifi_wep_key_lens,05"; break;
-                case PasswordType.WEP128: passtype = "wifi_wep_key_lens,0D"; break;
-                case PasswordType.WPA2: passtype = "wifi_wpa_psk_raw,"; break;
-                case PasswordType.WPA_TEXT: passtype = "wifi_wpa_psk_text,"; break;
-                default: throw new Exception();
-            }
-
-            switch (securityMode) {
-                case SecurityMode.none: smode = "0"; break;
-                case SecurityMode.WEP: smode = "1"; break;
-                case SecurityMode.WPA2Personal: smode = "2"; break;
-                default: throw new Exception();
-            }
-
-            switch (radioMode) {
-                case RadioMode.IDLE: radio = "0"; break;
-                case RadioMode.STA: radio = "1"; break;
-                case RadioMode.IBSS: radio = "2"; break;
-                case RadioMode.MiniAP: radio = "3"; break;
-                default: throw new Exception();
-            }
-
-            StopWorker();
-
-            Erase();
-            SendATCommand(Command.SSID + network);
-            SendATCommand(Command.SetValue + "wifi_mode," + radio);
-            SendATCommand(Command.SetValue + "wifi_priv_mode," + smode);
-            SendATCommand(Command.SetValue + passtype + password);
-            SendATCommand(Command.SaveConfig);
-            Reset();
+            this.SendATCommand("AT.S+WIFI=0");
+            this.SendATCommand("AT+S.SCFG=wifi_mode,1");
+            this.SendATCommand("AT+S.SCFG=wifi_priv_mode,2");
+            this.SendATCommand("AT+S.SSIDTXT=" + network);
+            this.SendATCommand("AT+S.SCFG=wifi_wpa_psk_text," + password);
+            this.SendATCommand("AT.S+WIFI=1");
+            this.SendATCommand("AT.S+WCFG");
+            this.Reset();
 
             this.ExtractLine(out var status1);
             this.ExtractLine(out var status2);
@@ -134,298 +36,20 @@ namespace GHIElectronics.TinyCLR.Networking.SPWF01Sx {
             this.ExtractLine(out var status13);
             this.ExtractLine(out var status14);
 
-            StartWorker();
+            this.StartWorker();
         }
 
-        public void ConnectMiniAP(string network, PasswordType ptype, string password, SecurityMode mode1) {
-            string passtype, smode;
-
-            Erase();
-            Thread.Sleep(100);
-            // set Password Type
-            if (ptype == PasswordType.WEP64) {
-                passtype = "05";
-            }
-
-            else if (ptype == PasswordType.WEP128) {
-                passtype = "0D";
-            }
-
-            else if (ptype == PasswordType.OPEN) {
-                passtype = "";
-            }
-
-            else if (ptype == PasswordType.WPA2) {
-                throw new ArgumentException("Only WEP64 and WEP128 are supported", "command");
-            }
-
-            else if (ptype == PasswordType.WPA_TEXT) {
-                throw new ArgumentException("Only WEP64 and WEP128 are supported", "command");
-            }
-            else {
-                throw new Exception();
-            }
-
-            // set Security Mode
-            if (mode1 == SecurityMode.none) {
-                smode = "0";
-                password = "";
-            }
-
-            else if (mode1 == SecurityMode.WEP) {
-                smode = "1";
-
-            }
-
-            else if (mode1 == SecurityMode.WPA2Personal) {
-                throw new ArgumentException("Only OPEN or WEP are supported", "command");
-            }
-            else {
-                throw new Exception();
-            }
-
-            if (ptype == PasswordType.WEP64 && password.Length > 5 || ptype == PasswordType.WEP128 && password.Length > 13) {
-                throw new ArgumentException("A maximum of5 text characters can be entered for 64 bit keys, and a maximum of 13 characters for 128 bit keys");
-            }
-
-
-
-            SendATCommand(Command.SSID + network);
-            SendATCommand(Command.SetValue + "wifi_wep_keys[0]," + ToHex(password));
-            SendATCommand(Command.SetValue + "wifi_wep_key_lens," + passtype);
-            SendATCommand("AT+S.SCFG=wifi_auth_type,0");
-            SendATCommand(Command.SetValue + "wifi_mode,3");
-            SendATCommand(Command.SetValue + "wifi_priv_mode," + smode);
-            SendATCommand(Command.SaveConfig);
-            Reset();
-
-            Thread.Sleep(100);
-
-
-        }
-        private static string[] decToHex = new[] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F" };
-        public static string ToHex(string hex) {
-            var builder = new StringBuilder();
-            //var res = "";
-
-            for (var i = 0; i < hex.Length; i++) {
-                var c = hex[i];
-                var n = (int)c;
-                var low = n & 0x0F;
-                var high = (n & 0xF0) >> 4;
-                //res = decToHex[high] + decToHex[low] + ":";
-                builder.Append(high);
-                builder.Append(low);
-            }
-
-            return builder.ToString();
-        }
-
-        public void WiFiOn() => SendATCommand("AT+S.WIFI=1");
-
-        public void WiFiOff() => SendATCommand("AT+S.WIFI=0");
-
-        public void Test() => SendATCommand(Command.Test);
-
-
-        public void CLoseServerSocket() => SendATCommand(Command.ServerSocket + "0");
-
-        public void Statistics() => SendATCommand(Command.Status);
-
-        public void ScanNetworks() => SendATCommand(Command.ScanNetworks);
-
-        public void FWUpdate(string IP, string filepath, string port) => SendATCommand(Command.FWUpdate + IP + "," + filepath + "," + port);
-
-
-        public void WriteToSocket(string id, string message) {
-            WriteData("AT+S.SOCKW=" + id + "," + message.Length.ToString(), message);
-
-
-            while (this.connected == true) {
-                SendATCommand("AT+S.SOCKQ=" + id);
-                Thread.Sleep(3000);
-
-                Debug.WriteLine(this.readline);
-
-
-                SendATCommand("AT+S.SOCKR=" + id + "," + this.readline);
-
-                Thread.Sleep(5000);
-
-                if (this.wait > 20) {
-                    Debug.WriteLine("Something wrong with connection.");
-
-                    break;
-                }
-
-                if (this.error == true) {
-                    Debug.WriteLine("Something wrong with connection.");
-
-                    break;
-                }
-
-
-            }
-
-
-
-        }
-
-
-
-        public void TLSAnonymous(string time, string domain, int socket) {
-            SendATCommand(Command.Clean);
-            Thread.Sleep(100);
-
-            SendATCommand(Command.SETTIME + "=" + time);
-            Thread.Sleep(100);
-
-            SendATCommand(Command.SETTIME);
-            Thread.Sleep(100);
-
-            SendATCommand(Command.OpenSocket + domain + "," + socket.ToString() + ",s");
-            Thread.Sleep(100);
-        }
-
-        public void TLSOneWayAuth(string time, byte[] cert, string domain, string hostname, int socket) {
-
-            if (cert.Length < 700 || cert.Length > 2000) {
-                throw new ArgumentException("Use certificate according RSA-2048 authentication");
-            }
-
-            SendATCommand(Command.Clean);
-            Thread.Sleep(100);
-
-            SendATCommand(Command.SETTIME + "=" + time);
-            Thread.Sleep(100);
-
-            SendATCommand(Command.SETTIME);
-            Thread.Sleep(100);
-
-            WriteData(Command.CertCA + cert.Length.ToString(), cert);
-            Thread.Sleep(100);
-
-            SendATCommand(Command.SetDomain + domain);
-            Thread.Sleep(100);
-
-            SendATCommand(Command.CheckCerts);
-            Thread.Sleep(100);
-
-            SendATCommand(Command.OpenSocket + hostname + "," + socket.ToString() + ",s,ind");
-            Thread.Sleep(100);
-
-        }
-
-        public void TLSMutualAuth(string time, byte[] cert, byte[] client, byte[] key, string domain, string hostname, int socket) {
-
-
-            SendATCommand(Command.Clean);
-            Thread.Sleep(100);
-
-            SendATCommand(Command.SETTIME + "=" + time);
-            Thread.Sleep(100);
-
-            SendATCommand(Command.SETTIME);
-            Thread.Sleep(100);
-
-            WriteData(Command.CertCA + cert.Length.ToString(), cert);
-            Thread.Sleep(100);
-
-            WriteData(Command.CertClient + client.Length.ToString(), client);
-            Thread.Sleep(100);
-
-            WriteData(Command.ClientKey + key.Length.ToString(), key);
-            Thread.Sleep(100);
-
-            SendATCommand(Command.SetDomain + domain);
-            Thread.Sleep(100);
-
-            SendATCommand(Command.CheckCerts);
-            Thread.Sleep(100);
-
-            SendATCommand(Command.OpenSocket + hostname + "," + socket.ToString() + ",s,ind");
-            Thread.Sleep(1000);
-
-        }
-
-        public void Ping(string host) => SendATCommand(Command.Ping + host);
-
-        //public void ReadSocket(string id, string length) => SendATCommand(Command.ReadSocket + id + "," + length);
-
-        //public void CloseSocket(string id) {
-        //
-        //    SendATCommand("AT+S.SOCKQ=" + id);
-        //    Thread.Sleep(3000);
-        //
-        //    SendATCommand("AT+S.SOCKR=" + id + "," + this.readline.ToString());
-        //    Thread.Sleep(5000);
-        //
-        //    SendATCommand(Command.CloseSocket + id);
-        //
-        //}
-
-        public void Erase() => SendATCommand(Command.Erase);
-
-        public void Reset() => SendATCommand(Command.Reset);
-
-        public void ServerSocket(int number) => SendATCommand(Command.Socket + number);
-
-        public void OpenSocket(string host, int port, SocketType sock) {
-            if (sock == SocketType.Secure) {
-
-                this.socket = "s";
-            }
-
-            else if (sock == SocketType.TCP) {
-                this.socket = "t";
-            }
-
-            else if (sock == SocketType.UDP) {
-                this.socket = "u";
-            }
-
-            SendATCommand(Command.OpenSocket + host + "," + port + "," + this.socket);
-        }
-
-        public void Help() => SendATCommand("AT+S.HELP");
-
-        public void Config() => SendATCommand("AT&V");
-
-        public void FileList() => SendATCommand("AT+S.FSL");
-
-        public void FileContent(string filepath) => SendATCommand("AT+S.FSP=" + filepath);
-
-        public void WriteData(string command, byte[] data) {
-            this.SendATCommand(command);
-            this.serWriter.WriteBytes(data);
-            this.Flush();
-        }
-
-        public void WriteData(string command, string data) {
-            this.SendATCommand(command);
-            this.serWriter.WriteString(data);
-            this.Flush();
-        }
-
-
-
-        public void ConfigureTls(int time, string domain, byte[] certificate) {
+        public void ConfigureTls(byte[] certificate) {
             this.StopWorker();
 
-            SendATCommand($"AT+S.SETTIME=" + time);
-            SendATCommand("AT+S.TLSCERT2=clean,all");
-            SendATCommand($"AT+S.TLSDOMAIN=f_domain," + domain);
-            SendATCommand($"AT+S.TLSCERT=f_ca," + certificate.Length);
+            this.SendATCommand("AT+S.TLSCERT=content,1");
+            this.SendATCommand($"AT+S.TLSCERT=ca," + certificate.Length);
             this.Write(certificate);
 
             this.ExtractLine(out var status1);
             this.ExtractLine(out var status2);
             this.ExtractLine(out var status3);
             this.ExtractLine(out var status4);
-            this.ExtractLine(out var status5);
-            this.ExtractLine(out var status6);
-            this.ExtractLine(out var status7);
-            this.ExtractLine(out var status8);
 
             this.StartWorker();
         }
@@ -800,7 +424,7 @@ namespace GHIElectronics.TinyCLR.Networking.SPWF01Sx {
             this.StopWorker();
 
             this.SendATCommand("AT+S.SCFG=console1_hwfc,1");
-            this.SendATCommand(Command.SaveConfig);
+            this.SendATCommand("AT+S.WCFG");
 
             this.Reset();
 
