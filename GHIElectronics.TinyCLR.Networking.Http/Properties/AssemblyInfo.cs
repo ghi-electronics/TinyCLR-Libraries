9d22a24fc587eca4e7a75bca0a3bfff964210562
John Brochue <john.brochue@ghielectronics.com>
2018-02-05 12:01:51 -0500

    Added skeleton networking projects, #134

---
+++ GHIElectronics.TinyCLR.Networking.Http/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GHIElectronics.TinyCLR.Networking.Http")]
+[assembly: AssemblyDescription("TinyCLR OS HTTP library.")]
+[assembly: AssemblyCompany("GHI Electronics, LLC")]
+[assembly: AssemblyProduct("TinyCLR OS")]
+[assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2018")]
+[assembly: ComVisible(false)]
+[assembly: Guid("D89B51CA-0A2C-4A50-8312-D3B3F57F1096")]
+[assembly: AssemblyVersion("0.8.0.0")]
+[assembly: AssemblyFileVersion("0.8.0.0")]
---
+++ GHIElectronics.TinyCLR.Networking/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GHIElectronics.TinyCLR.Networking")]
+[assembly: AssemblyDescription("TinyCLR OS networking library.")]
+[assembly: AssemblyCompany("GHI Electronics, LLC")]
+[assembly: AssemblyProduct("TinyCLR OS")]
+[assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2018")]
+[assembly: ComVisible(false)]
+[assembly: Guid("1AFA3955-B36D-4A8A-8B7F-86F39FC2FDE3")]
+[assembly: AssemblyVersion("0.8.0.0")]
+[assembly: AssemblyFileVersion("0.8.0.0")]
