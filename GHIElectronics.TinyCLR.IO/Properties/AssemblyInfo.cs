91dcf46dc9119cf96e8d0f99677bacc47b4d3951
John Brochue <john.brochue@ghielectronics.com>
2018-02-14 14:33:22 -0500

    Added empty IO project

---
+++ GHIElectronics.TinyCLR.IO/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GHIElectronics.TinyCLR.IO")]
+[assembly: AssemblyDescription("TinyCLR OS IO library.")]
+[assembly: AssemblyCompany("GHI Electronics, LLC")]
+[assembly: AssemblyProduct("TinyCLR OS")]
+[assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2018")]
+[assembly: ComVisible(false)]
+[assembly: Guid("313AEC8E-2B47-4A1A-ACE2-A766D076B2AE")]
+[assembly: AssemblyVersion("0.8.0.0")]
+[assembly: AssemblyFileVersion("0.8.0.0")]
