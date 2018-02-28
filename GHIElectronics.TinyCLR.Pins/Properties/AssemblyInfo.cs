ee7e08cb4697d881f85be1e356e7e23ab31b3ed7
John Brochue <johnbrochue@outlook.com>
2018-02-28 12:59:49 -0500

    Updated versions

--- GHIElectronics.TinyCLR.Pins/Properties/AssemblyInfo.cs
+++ GHIElectronics.TinyCLR.Pins/Properties/AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2018")]
 [assembly: ComVisible(false)]
 [assembly: Guid("2DCEEF58-5455-457C-B968-52D791605D07")]
-[assembly: AssemblyVersion("0.8.0.0")]
-[assembly: AssemblyFileVersion("0.8.0.0")]
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
