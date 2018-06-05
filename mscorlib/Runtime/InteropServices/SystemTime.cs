e10181d75b2b97a135efe359fc99687e87f3f90b
John Brochue <johnbrochue@outlook.com>
2018-06-05 13:32:14 -0400

    Marked a member not cls compliant, fixes #217

--- mscorlib/Runtime/InteropServices/SystemTime.cs
+++ mscorlib/Runtime/InteropServices/SystemTime.cs
@@ -3,6 +3,7 @@
 namespace System.Runtime.InteropServices {
     public static class SystemTime {
         [MethodImpl(MethodImplOptions.InternalCall)]
+        [CLSCompliant(false)]
         public static extern void SetSystemTime(ulong utcTicks, int timezoneOffsetMinutes);
     }
 }
