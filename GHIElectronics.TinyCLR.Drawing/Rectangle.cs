883de0deb1d2fb39df59d7a74e1a927d61cf6794
John Brochue <johnbrochue@outlook.com>
2018-06-08 15:05:46 -0400

    Add rectangle

---
+++ GHIElectronics.TinyCLR.Drawing/Rectangle.cs
@@ -0,0 +1,15 @@
+﻿namespace System.Drawing {
+    public struct Rectangle {
+        public Rectangle(int x, int y, int width, int height) {
+            this.X = x;
+            this.Y = y;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+}
