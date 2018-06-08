25a9301cae9189f5c2a59b286f42d35f988d137e
John Brochue <johnbrochue@outlook.com>
2018-06-08 12:28:23 -0400

    Added SizeF struct

---
+++ GHIElectronics.TinyCLR.Drawing/SizeF.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace System.Drawing {
+    public struct SizeF {
+        public static readonly SizeF Empty = new SizeF();
+
+        public SizeF(SizeF size) {
+            this.Width = size.Width;
+            this.Height = size.Height;
+        }
+
+        public SizeF(float width, float height) {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public bool IsEmpty => this.Width == 0 && this.Height == 0;
+
+        public float Width { get; set; }
+        public float Height { get; set; }
+    }
+}
