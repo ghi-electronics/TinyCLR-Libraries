0e0866ba3560324c0186ceaca1bbd0a213c7a033
John Brochue <johnbrochue@outlook.com>
2018-06-08 15:36:21 -0400

    Add StringFormat

---
+++ GHIElectronics.TinyCLR.Drawing/StringFormat.cs
@@ -0,0 +1,21 @@
+﻿namespace System.Drawing {
+    public enum StringAlignment {
+        Near = 0,
+        Center = 1,
+        Far = 2
+    }
+
+    public enum StringTrimming {
+        None = 0,
+        Character = 1,
+        Word = 2,
+        EllipsisCharacter = 3,
+        EllipsisWord = 4,
+        EllipsisPath = 5
+    }
+
+    public sealed class StringFormat {
+        public StringAlignment Alignment { get; set; }
+        public StringTrimming Trimming { get; set; }
+    }
+}
