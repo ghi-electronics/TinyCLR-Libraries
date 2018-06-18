dab70c0662f8ab8a8e50dfbd45a2d3113daf7fe0
John Brochue <johnbrochue@outlook.com>
2018-06-18 15:24:32 -0400

    Added images for keyboard

--- GHIElectronics.TinyCLR.UI/UI/OnScreenKeyboard.cs
+++ GHIElectronics.TinyCLR.UI/UI/OnScreenKeyboard.cs
@@ -1,16 +1,23 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Collections;
+using System.Drawing;
 using GHIElectronics.TinyCLR.UI.Controls;
 using GHIElectronics.TinyCLR.UI.Input;
 using GHIElectronics.TinyCLR.UI.Media;
+using GHIElectronics.TinyCLR.UI.Media.Imaging;
 
 namespace GHIElectronics.TinyCLR.UI {
     public class OnScreenKeyboard : Window {
+        private Hashtable views;
         private TextBox source;
         private TextBox input;
+        private Controls.Image image;
 
         public static Font Font { get; set; }
 
         internal OnScreenKeyboard() {
+            this.views = new Hashtable();
+
             this.Width = WindowManager.Instance.ActualWidth;
             this.Height = WindowManager.Instance.ActualHeight;
             this.Background = new SolidColorBrush(Colors.Red);
@@ -24,6 +31,14 @@ namespace GHIElectronics.TinyCLR.UI {
 
             holder.Children.Add(this.input);
 
+            this.image = new Controls.Image {
+                Source = null
+            };
+
+            this.image.TouchUp += this.OnTouchUp;
+
+            holder.Children.Add(this.image);
+
             this.Child = holder;
         }
 
@@ -33,9 +48,11 @@ namespace GHIElectronics.TinyCLR.UI {
 
             this.source = textBox;
             this.input.Text = this.source.Text;
+
+            this.ShowView(KeyboardViewId.Lowercase);
         }
 
-        protected override void OnTouchUp(TouchEventArgs e) {
+        private void OnTouchUp(object sender, TouchEventArgs e) {
             if (e.Touches[0].Y < 100)
                 this.Close();
             else
@@ -48,6 +65,43 @@ namespace GHIElectronics.TinyCLR.UI {
             this.source.Text = this.input.Text;
 
             Application.Current.CloseOnScreenKeyboard();
+        }
+
+        private void ShowView(KeyboardViewId id) {
+            if (!this.views.Contains(id))
+                this.CreateView(id);
+
+            var view = (KeyboardView)this.views[id];
+
+            this.image.Source = view.Image;
+
+            this.image.InvalidateMeasure();
+        }
+
+        private void CreateView(KeyboardViewId id) {
+            System.Drawing.Bitmap bmp = default;
+
+            switch (id) {
+                case KeyboardViewId.Lowercase: bmp = Resources.GetBitmap(Resources.BitmapResources.Keyboard_Lowercase); break;
+                case KeyboardViewId.Uppercase: bmp = Resources.GetBitmap(Resources.BitmapResources.Keyboard_Uppercase); break;
+                case KeyboardViewId.Numbers: bmp = Resources.GetBitmap(Resources.BitmapResources.Keyboard_Numbers); break;
+                case KeyboardViewId.Symbols: throw new NotImplementedException(); // bmp = Resources.GetBitmap(Resources.BitmapResources.Keyboard_Symbols); break;
+            }
+
+            this.views.Add(id, new KeyboardView {
+                Image = BitmapImage.FromGraphics(Graphics.FromImage(bmp))
+            });
+        }
+
+        private enum KeyboardViewId {
+            Lowercase,
+            Uppercase,
+            Numbers,
+            Symbols
+        }
+
+        private class KeyboardView {
+            public BitmapImage Image { get; set; }
         }
     }
 }
