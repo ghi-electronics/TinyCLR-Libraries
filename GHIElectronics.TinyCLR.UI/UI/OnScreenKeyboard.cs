766abbeab3c86acb3bab418700df5e2905019baf
John Brochue <johnbrochue@outlook.com>
2018-06-19 10:31:01 -0400

    Implemented key detection logic

--- GHIElectronics.TinyCLR.UI/UI/OnScreenKeyboard.cs
+++ GHIElectronics.TinyCLR.UI/UI/OnScreenKeyboard.cs
@@ -12,6 +12,11 @@ namespace GHIElectronics.TinyCLR.UI {
         private TextBox source;
         private TextBox input;
         private Controls.Image image;
+        private KeyboardView active;
+        private double scaleX;
+        private double scaleY;
+        private int offsetX;
+        private int offsetY;
 
         public static Font Font { get; set; }
 
@@ -56,12 +61,28 @@ namespace GHIElectronics.TinyCLR.UI {
         }
 
         private void OnTouchUp(object sender, TouchEventArgs e) {
-            if (e.Touches[0].Y < 100)
-                this.Close();
-            else
-                this.Append('a');
+            var x = (int)(e.Touches[0].X * this.scaleX - this.offsetX);
+            var y = (int)(e.Touches[0].Y * this.scaleY - this.offsetY);
+            var row = y / this.active.RowHeight;
+            var column = 0;
+            var columnWidth = this.active.ColumnWidth[row];
+            var total = this.active.RowColumnOffset[row];
+
+            while (column < columnWidth.Length && total < x)
+                total += columnWidth[column++];
+
+            if (--column < 0 || total < x)
+                return;
+
+            if (this.active.SpecialKeys?[row]?[column] is Action a) {
+                a();
+            }
+            else {
+                this.Append(this.active.Keys[row][column]);
+            }
         }
 
+        private void Backspace() => this.input.Text = this.input.Text.Substring(0, this.input.Text.Length - 1);
         private void Append(char c) => this.input.Text += c;
 
         private void Close() {
@@ -74,26 +95,128 @@ namespace GHIElectronics.TinyCLR.UI {
             if (!this.views.Contains(id))
                 this.CreateView(id);
 
-            var view = (KeyboardView)this.views[id];
+            this.active = (KeyboardView)this.views[id];
 
-            this.image.Source = view.Image;
+            this.image.Source = this.active.Image;
 
             this.image.InvalidateMeasure();
+
+            this.scaleX = this.active.Image.Width / (double)this.image.Width;
+            this.scaleY = this.active.Image.Height / (double)this.image.Height;
+            this.offsetX = 0;
+            this.offsetY = OnScreenKeyboard.Font.Height;
         }
 
         private void CreateView(KeyboardViewId id) {
-            System.Drawing.Bitmap bmp = default;
+            var hf = 16;
+            var sz = 32;
+            var szh = 48;
+            var full = new[] { sz, sz, sz, sz, sz, sz, sz, sz, sz, sz };
+            var image = default(System.Drawing.Bitmap);
+            var view = new KeyboardView { RowHeight = sz };
 
             switch (id) {
-                case KeyboardViewId.Lowercase: bmp = Resources.GetBitmap(Resources.BitmapResources.Keyboard_Lowercase); break;
-                case KeyboardViewId.Uppercase: bmp = Resources.GetBitmap(Resources.BitmapResources.Keyboard_Uppercase); break;
-                case KeyboardViewId.Numbers: bmp = Resources.GetBitmap(Resources.BitmapResources.Keyboard_Numbers); break;
-                case KeyboardViewId.Symbols: throw new NotImplementedException(); // bmp = Resources.GetBitmap(Resources.BitmapResources.Keyboard_Symbols); break;
+                case KeyboardViewId.Lowercase:
+                    image = Resources.GetBitmap(Resources.BitmapResources.Keyboard_Lowercase);
+                    view.RowColumnOffset = new[] { 0, hf, 0, 0 };
+                    view.ColumnWidth = new[] {
+                        full,
+                        new[] { sz, sz, sz, sz, sz, sz, sz, sz, sz },
+                        new[] { szh, sz, sz, sz, sz, sz, sz, sz, szh },
+                        new[] { szh, sz, sz * 5, sz, szh }
+                    };
+                    view.Keys = new[] {
+                        new[] { 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p' },
+                        new[] { 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l' },
+                        new[] { '\0', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '\0' },
+                        new[] { '\0', ',', ' ', '.', '\0' }
+                    };
+                    view.SpecialKeys = new[] {
+                        null,
+                        null,
+                        new Action[] { () => this.ShowView(KeyboardViewId.Uppercase), null, null, null, null, null, null, null, () => this.Backspace() },
+                        new Action[] { () => this.ShowView(KeyboardViewId.Numbers), null, null, null, () => this.Close() }
+                    };
+
+                    break;
+
+                case KeyboardViewId.Uppercase:
+                    image = Resources.GetBitmap(Resources.BitmapResources.Keyboard_Uppercase);
+                    view.RowColumnOffset = new[] { 0, hf, 0, 0 };
+                    view.ColumnWidth = new[] {
+                        full,
+                        new[] { sz, sz, sz, sz, sz, sz, sz, sz, sz },
+                        new[] { szh, sz, sz, sz, sz, sz, sz, sz, szh },
+                        new[] { szh, sz, sz * 5, sz, szh }
+                    };
+                    view.Keys = new[] {
+                        new[] { 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P' },
+                        new[] { 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L' },
+                        new[] { '\0', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '\0' },
+                        new[] { '\0', ',', ' ', '.', '\0' }
+                    };
+                    view.SpecialKeys = new[] {
+                        null,
+                        null,
+                        new Action[] { () => this.ShowView(KeyboardViewId.Lowercase), null, null, null, null, null, null, null, () => this.Backspace() },
+                        new Action[] { () => this.ShowView(KeyboardViewId.Numbers), null, null, null, () => this.Close() }
+                    };
+
+                    break;
+
+                case KeyboardViewId.Numbers:
+                    image = Resources.GetBitmap(Resources.BitmapResources.Keyboard_Numbers);
+                    view.RowColumnOffset = new[] { 0, 0, 0, 0 };
+                    view.ColumnWidth = new[] {
+                        full,
+                        full,
+                        new[] { szh, sz, sz, sz, sz, sz, sz, sz, szh },
+                        new[] { szh, sz, sz * 5, sz, szh }
+                    };
+                    view.Keys = new[] {
+                        new[] { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' },
+                        new[] { '@', '#', '$', '%', '&', '*', '-', '+', '(', ')' },
+                        new[] { '\0', '!', '"', '\'', ':', ';', '/', '?', '\0' },
+                        new[] { '\0', ',', ' ', '.', '\0' }
+                    };
+                    view.SpecialKeys = new[] {
+                        null,
+                        null,
+                        new Action[] { () => this.ShowView(KeyboardViewId.Symbols), null, null, null, null, null, null, null, () => this.Backspace() },
+                        new Action[] { () => this.ShowView(KeyboardViewId.Lowercase), null, null, null, () => this.Close() }
+                    };
+
+                    break;
+
+                case KeyboardViewId.Symbols:
+                    //image = Resources.GetBitmap(Resources.BitmapResources.Keyboard_Symbols);
+                    image = Resources.GetBitmap(Resources.BitmapResources.Keyboard_Numbers);
+                    view.RowColumnOffset = new[] { 0, 0, 0, 0 };
+                    view.ColumnWidth = new[] {
+                        full,
+                        full,
+                        new[] { szh, sz, sz, sz, sz, sz, sz, sz, szh },
+                        new[] { szh, sz, sz * 5, sz, szh }
+                    };
+                    view.Keys = new[] {
+                        new[] { '~', '`', '|', '•', '√', 'π', '÷', '×', '{', '}' },
+                        new[] { '\t', '£', '¢', '€', 'º', '^', '_', '=', '[', ']' },
+                        new[] { '\0', '™', '®', '©', '¶', '\\', '<', '>', '\0' },
+                        new[] { '\0', ',', ' ', '.', '\0' }
+                    };
+                    view.SpecialKeys = new[] {
+                        null,
+                        null,
+                        new Action[] { () => this.ShowView(KeyboardViewId.Numbers), null, null, null, null, null, null, null, () => this.Backspace() },
+                        new Action[] { () => this.ShowView(KeyboardViewId.Lowercase), null, null, null, () => this.Close() }
+                    };
+
+                    break;
             }
 
-            this.views.Add(id, new KeyboardView {
-                Image = BitmapImage.FromGraphics(Graphics.FromImage(bmp))
-            });
+            view.Image = BitmapImage.FromGraphics(Graphics.FromImage(image));
+
+            this.views.Add(id, view);
         }
 
         private enum KeyboardViewId {
@@ -105,6 +228,11 @@ namespace GHIElectronics.TinyCLR.UI {
 
         private class KeyboardView {
             public BitmapImage Image { get; set; }
+            public int RowHeight { get; set; }
+            public int[] RowColumnOffset { get; set; }
+            public int[][] ColumnWidth { get; set; }
+            public char[][] Keys { get; set; }
+            public Action[][] SpecialKeys { get; set; }
         }
     }
 }
