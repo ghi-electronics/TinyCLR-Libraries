8af225391fe75579fd35dfe278cc6ae53ce6fa10
Dat <dat.tran@ghielectronics.com>
2018-06-26 10:59:07 -0400

    Add SDCard managed.

---
+++ GHIElectronics.TinyCLR.Devices/SdCardController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using GHIElectronics.TinyCLR.Devices.SdCard.Provider;
+
+namespace GHIElectronics.TinyCLR.Devices.SdCard {
+    public sealed class SdCardController {
+        private ISdCardControllerProvider provider;
+
+        internal SdCardController(ISdCardControllerProvider provider) => this.provider = provider;
+
+        public static SdCardController GetDefault() => new SdCardController(Api.ParseSelector(Api.GetDefaultSelector(ApiType.SdCardProvider), out var providerId, out var idx) ? SdCardProvider.FromId(providerId).GetControllers()[idx] : null);
+
+        public static SdCardController[] GetControllers(ISdCardProvider provider) {
+
+            var providers = provider.GetControllers();
+
+            var controllers = new SdCardController[providers.Length];
+
+            for (var i = 0; i < providers.Length; ++i) {
+                controllers[i] = new SdCardController(providers[i]);
+            }
+
+            return controllers;
+
+        }
+
+        public IntPtr Hdc => this.provider.Hdc;
+
+        public int ControllerIndex => this.provider.ControllerIndex;
+
+        public ISdCardControllerProvider Provider => this.provider;
+    }
+}
---
+++ GHIElectronics.TinyCLR.Devices/SdCardProvider.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace GHIElectronics.TinyCLR.Devices.SdCard.Provider {
+    public interface ISdCardProvider {
+        ISdCardControllerProvider[] GetControllers();
+    }
+
+    public interface ISdCardControllerProvider {
+        IntPtr Hdc { get; }
+
+        int ControllerIndex { get; }
+    }
+
+    public interface ISdCardLowLevelController {
+        int ReadSectors(int sector, int count, byte[] buffer, int offset, int timeout);
+        int WriteSectors(int sector, int count, byte[] buffer, int offset, int timeout);
+        int EraseSectors(int sector, int count, int timeout);
+        void GetSectorMap(out int[] sizes, out int count, out bool uniform);
+    }
+
+    public class SdCardProvider : ISdCardProvider {
+        private ISdCardControllerProvider[] controllers;
+
+        private static Hashtable providers = new Hashtable();
+
+        public string Name { get; }
+
+        public ISdCardControllerProvider[] GetControllers() => this.controllers;
+
+        private SdCardProvider(string name) {
+
+            this.Name = name;
+
+            var api = Api.Find(this.Name, ApiType.SdCardProvider);
+
+            this.controllers = new ISdCardControllerProvider[DefaultSdCardControllerProvider.GetControllerCount(api.Implementation)];
+
+            for (var i = 0; i < this.controllers.Length; i++)
+
+                this.controllers[i] = new DefaultSdCardControllerProvider(api.Implementation, i);
+        }
+
+        public static ISdCardProvider FromId(string id) {
+
+            if (SdCardProvider.providers.Contains(id))
+                return (ISdCardProvider)SdCardProvider.providers[id];
+
+            var res = new SdCardProvider(id);
+
+            SdCardProvider.providers[id] = res;
+
+            return res;
+        }
+
+    }
+
+    internal class DefaultSdCardControllerProvider : ISdCardControllerProvider, ISdCardLowLevelController {
+
+#pragma warning disable CS0169
+        private readonly IntPtr nativeProvider;
+        private readonly int idx;
+#pragma warning restore CS0169
+
+        internal DefaultSdCardControllerProvider(IntPtr nativeProvider, int idx) {
+            this.nativeProvider = nativeProvider;
+            this.idx = idx;
+        }
+
+        public IntPtr Hdc => this.nativeProvider;
+        public int ControllerIndex => this.idx;
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        internal extern static int GetControllerCount(IntPtr nativeProvider);
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        public extern int ReadSectors(int sector, int count, byte[] buffer, int offset, int timeout);
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        public extern int WriteSectors(int sector, int count, byte[] buffer, int offset, int timeout);
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        public extern int EraseSectors(int sector, int count, int timeout);
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        public extern void GetSectorMap(out int[] sizes, out int count, out bool uniform);
+
+    }
+}
