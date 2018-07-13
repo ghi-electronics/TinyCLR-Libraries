bdc2928136f29221e5a5aafd1f8a429b94f96ca6
John Brochue <johnbrochue@outlook.com>
2018-07-13 13:13:25 -0400

    Updated API ids to latest

--- mscorlib/Runtime/InteropServices/Api.cs
+++ mscorlib/Runtime/InteropServices/Api.cs
@@ -1,36 +1,50 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Collections;
+using System.Runtime.CompilerServices;
 
 namespace System.Runtime.InteropServices {
     //Keep in sync with native
     [CLSCompliant(false)]
     public enum ApiType : uint {
-        ApiProvider = 0,
-        DebuggerProvider = 1,
-        InteropProvider = 2,
-        MemoryProvider = 3,
-        TaskProvider = 4,
-        SystemTimeProvider = 5,
-        DeploymentProvider = 0 | 0x20000000,
-        InterruptProvider = 1 | 0x20000000,
-        PowerProvider = 2 | 0x20000000,
-        NativeTimeProvider = 3 | 0x20000000,
-        AdcProvider = 0 | 0x40000000,
-        CanProvider = 1 | 0x40000000,
-        DacProvider = 2 | 0x40000000,
-        DisplayProvider = 3 | 0x40000000,
-        GpioProvider = 4 | 0x40000000,
-        I2cProvider = 5 | 0x40000000,
-        PwmProvider = 6 | 0x40000000,
-        RtcProvider = 7 | 0x40000000,
-        SpiProvider = 8 | 0x40000000,
-        UartProvider = 9 | 0x40000000,
-        UsbClientProvider = 10 | 0x40000000,
-        SdCardProvider = 11 | 0x40000000,
+        ApiManager = 0,
+        DebuggerManager = 1,
+        InteropManager = 2,
+        MemoryManager = 3,
+        TaskManager = 4,
+        SystemTimeManager = 5,
+        DeploymentController = 0 | 0x20000000,
+        InterruptController = 1 | 0x20000000,
+        PowerController = 2 | 0x20000000,
+        NativeTimeController = 3 | 0x20000000,
+        AdcController = 0 | 0x40000000,
+        CanController = 1 | 0x40000000,
+        DacController = 2 | 0x40000000,
+        DcmiController = 3 | 0x40000000,
+        DisplayController = 4 | 0x40000000,
+        EthernetMacController = 5 | 0x40000000,
+        GpioController = 6 | 0x40000000,
+        I2cController = 7 | 0x40000000,
+        I2sController = 8 | 0x40000000,
+        PwmController = 9 | 0x40000000,
+        RtcController = 10 | 0x40000000,
+        SaiController = 11 | 0x40000000,
+        SdCardController = 12 | 0x40000000,
+        SpiController = 13 | 0x40000000,
+        UartController = 14 | 0x40000000,
+        UsbClientController = 15 | 0x40000000,
+        UsbHostController = 16 | 0x40000000,
+        WatchdogController = 17 | 0x40000000,
         Custom = 0 | 0x80000000,
     }
 
     public sealed class Api {
+        public delegate object DefaultCreator();
+
+        private static readonly Hashtable defaultCreators = new Hashtable();
+
         private Api() { }
+
+        public static object GetDefaultCreator(ApiType apiType) => Api.defaultCreators.Contains(apiType) ? ((DefaultCreator)Api.defaultCreators[apiType])?.Invoke() : null;
+        public static void SetDefaultCreator(ApiType apiType, DefaultCreator creator) => Api.defaultCreators[apiType] = creator;
 
         [MethodImpl(MethodImplOptions.InternalCall)]
         public static extern void Add(IntPtr address);
