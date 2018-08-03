a96b5c37099adb141c22575925ee643468cac00b
John Brochue <johnbrochue@outlook.com>
2018-08-03 13:51:07 -0400

    Added power class to native, fixes #252

---
+++ GHIElectronics.TinyCLR.Native/Power.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace GHIElectronics.TinyCLR.Native {
+    public enum PowerSleepLevel {
+        Sleep = 0,
+        Hibernate = 1,
+        Off = 2,
+    }
+
+    public static class Power {
+        public static void Reset() => Power.Reset(true);
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        public static extern void Reset(bool runCoreAfter);
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        public static extern void Sleep(PowerSleepLevel sleepLevel);
+    }
+}
