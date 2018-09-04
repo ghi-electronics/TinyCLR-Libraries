1371d7648abb1a074acce0321200e4f6c6acdf53
John Brochue <johnbrochue@outlook.com>
2018-09-04 12:08:59 -0400

    Implement managed stretch image and part of draw text

--- GHIElectronics.TinyCLR.Drawing/ManagedGraphics.cs
+++ GHIElectronics.TinyCLR.Drawing/ManagedGraphics.cs
@@ -71,11 +71,35 @@
         public void SetPixel(int x, int y, uint color) => this.drawTarget.SetPixel(x, y, new Color(color));
         public byte[] GetBitmap() => this.drawTarget.GetData();
 
+        public void DrawTextInRect(string text, int x, int y, int width, int height, uint dtFlags, Color color, Font font) => throw new NotSupportedException();
+
         public void DrawLine(uint color, int thickness, int x0, int y0, int x1, int y1) => throw new NotImplementedException();
         public void DrawRectangle(uint colorOutline, int thicknessOutline, int x, int y, int width, int height, int xCornerRadius, int yCornerRadius, uint colorGradientStart, int xGradientStart, int yGradientStart, uint colorGradientEnd, int xGradientEnd, int yGradientEnd, ushort opacity) => throw new NotImplementedException();
         public void DrawEllipse(uint colorOutline, int thicknessOutline, int x, int y, int xRadius, int yRadius, uint colorGradientStart, int xGradientStart, int yGradientStart, uint colorGradientEnd, int xGradientEnd, int yGradientEnd, ushort opacity) => throw new NotImplementedException();
-        public void DrawText(string text, Font font, uint color, int x, int y) => throw new NotImplementedException();
-        public void DrawTextInRect(string text, int x, int y, int width, int height, uint dtFlags, Color color, Font font) => throw new NotImplementedException();
-        public void StretchImage(int xDst, int yDst, int widthDst, int heightDst, IGraphics image, int xSrc, int ySrc, int widthSrc, int heightSrc, ushort opacity) => throw new NotImplementedException();
+
+        public void DrawText(string text, Font font, uint color, int x, int y) {
+            if (text == null) throw new ArgumentNullException(nameof(text));
+            if (font == null) throw new ArgumentNullException(nameof(font));
+            if (!font.IsGHIMono8x5) throw new NotSupportedException();
+
+            var hScale = font.Size / 8;
+            var vScale = hScale;
+
+            for (var i = 0; i < text.Length; i++) {
+
+            }
+        }
+
+        public void StretchImage(int xDst, int yDst, int widthDst, int heightDst, IGraphics image, int xSrc, int ySrc, int widthSrc, int heightSrc, ushort opacity) {
+            if (!(image is ManagedGraphics mg)) throw new NotSupportedException();
+
+            var dt = mg.drawTarget;
+
+            if (xSrc != 0 || ySrc != 0 || widthSrc != dt.Width || heightSrc != dt.Height || widthSrc != widthDst || heightSrc != heightDst || opacity != 0xFF) throw new NotSupportedException();
+
+            for (var y = 0; y < widthDst; y++)
+                for (var x = 0; x < widthDst; x++)
+                    this.SetPixel(x, y, mg.GetPixel(x, y));
+        }
     }
 }
