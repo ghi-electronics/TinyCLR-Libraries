312bc9a735062e5b1b56bf245c61e28cf30bd467
John Brochue <johnbrochue@outlook.com>
2018-09-04 12:01:06 -0400

    Implement RGB565 and part of managed graphics

--- GHIElectronics.TinyCLR.Drawing/ManagedGraphics.cs
+++ GHIElectronics.TinyCLR.Drawing/ManagedGraphics.cs
@@ -12,14 +12,47 @@
         public int Width { get; }
         public int Height { get; }
 
-        public Color GetPixel(int x, int y) => throw new NotImplementedException();
-        public void SetPixel(int x, int y, Color color) => throw new NotImplementedException();
-        public byte[] GetData() => throw new NotImplementedException();
+        public Color GetPixel(int x, int y) {
+            var idx = (y * this.Width + x) * 2;
+            var b1 = this.data[idx];
+            var b2 = this.data[idx + 1];
 
-        public void Flush() => throw new NotSupportedException();
-        public void Clear(Color color) => throw new NotSupportedException();
+            var r = (b1 & 0b1111_1000) << 0;
+            var g = ((b1 & 0b0000_0111) << 5) | ((b2 & 0b1110_0000) >> 3);
+            var b = (b2 & 0b0001_1111) << 3;
 
-        public void Dispose() => throw new NotSupportedException();
+            return Color.FromArgb(r, g, b);
+        }
+
+        public void SetPixel(int x, int y, Color color) {
+            var r = (color.R & 0b1111_1000) << 8;
+            var g = (color.G & 0b1111_1100) << 3;
+            var b = (color.B & 0b1111_1000) >> 3;
+            var c = r | g | b;
+
+            var idx = (y * this.Width + x) * 2;
+            var b1 = (byte)((c & 0xFF00) >> 8);
+            var b2 = (byte)((c & 0x00FF) >> 0);
+
+            this.data[idx] = b1;
+            this.data[idx + 1] = b2;
+        }
+
+        public byte[] GetData() => this.data;
+
+        public void Flush() {
+
+        }
+
+        public void Clear(Color color) {
+            if (color != Color.Black) throw new NotSupportedException();
+
+            Array.Clear(this.data, 0, this.data.Length);
+        }
+
+        public void Dispose() {
+
+        }
     }
 
     internal sealed class ManagedGraphics : IGraphics {
@@ -34,9 +67,9 @@
         public void Flush(IntPtr hdc) => this.drawTarget.Flush();
         public void Dispose() => this.drawTarget.Dispose();
 
-        public uint GetPixel(int x, int y) => throw new NotImplementedException();
-        public void SetPixel(int x, int y, uint color) => throw new NotImplementedException();
-        public byte[] GetBitmap() => throw new NotImplementedException();
+        public uint GetPixel(int x, int y) => (uint)this.drawTarget.GetPixel(x, y).ToArgb();
+        public void SetPixel(int x, int y, uint color) => this.drawTarget.SetPixel(x, y, new Color(color));
+        public byte[] GetBitmap() => this.drawTarget.GetData();
 
         public void DrawLine(uint color, int thickness, int x0, int y0, int x1, int y1) => throw new NotImplementedException();
         public void DrawRectangle(uint colorOutline, int thicknessOutline, int x, int y, int width, int height, int xCornerRadius, int yCornerRadius, uint colorGradientStart, int xGradientStart, int yGradientStart, uint colorGradientEnd, int xGradientEnd, int yGradientEnd, ushort opacity) => throw new NotImplementedException();
