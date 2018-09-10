afc3dc7a37a531475db92955b24698d522045c25
John Brochue <johnbrochue@outlook.com>
2018-09-10 16:29:56 -0400

    Added memory class to native, ghi-electronics/TinyCLR-Core#334

---
+++ GHIElectronics.TinyCLR.Native/Memory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace GHIElectronics.TinyCLR.Native {
+    public static class Memory {
+        public static IntPtr Allocate(long length) => Memory.Allocate((IntPtr)length);
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        public static extern IntPtr Allocate(IntPtr length);
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        public static extern void Free(IntPtr ptr);
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        public static extern void GetStats(out IntPtr used, out IntPtr free);
+
+        public static long UsedBytes { get { Memory.GetStats(out var used, out _); return used.ToInt64(); } }
+        public static long FreeBytes { get { Memory.GetStats(out _, out var free); return free.ToInt64(); } }
+    }
+}
