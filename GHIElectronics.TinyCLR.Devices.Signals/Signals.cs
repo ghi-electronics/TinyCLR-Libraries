90ef83209b411f9bd6ea5a691a22236baf8b17e9
John Brochue <johnbrochue@outlook.com>
2018-09-25 17:32:07 -0400

    Tweaked some default values, added more gpio control, and use TimeSpan for signals

--- GHIElectronics.TinyCLR.Devices.Signals/Signals.cs
+++ GHIElectronics.TinyCLR.Devices.Signals/Signals.cs
@@ -22,6 +22,7 @@ namespace GHIElectronics.TinyCLR.Devices.Signals {
         public TimeSpan PulseLength { get; set; }
         public GpioPinValue PulsePinValue { get; set; }
         public GpioPinValue EchoPinValue { get; set; }
+        public GpioPinDriveMode PulsePinDriveMode { get; set; }
         public GpioPinDriveMode EchoPinDriveMode { get; set; }
 
         public PulseFeedback(int pinNumber, PulseFeedbackMode mode)
@@ -47,6 +48,7 @@ namespace GHIElectronics.TinyCLR.Devices.Signals {
             this.PulseLength = TimeSpan.FromMilliseconds(20);
             this.PulsePinValue = GpioPinValue.High;
             this.EchoPinValue = GpioPinValue.High;
+            this.PulsePinDriveMode = GpioPinDriveMode.Input;
             this.EchoPinDriveMode = GpioPinDriveMode.Input;
 
             this.mode = mode;
@@ -69,41 +71,44 @@ namespace GHIElectronics.TinyCLR.Devices.Signals {
         }
 
         [MethodImpl(MethodImplOptions.InternalCall)]
-        public extern long GeneratePulse();
+        public extern TimeSpan GeneratePulse();
     }
 
     public sealed class SignalGenerator : IDisposable {
         private readonly IntPtr gpioApi;
         private readonly int pinNumber;
         private readonly GpioPin pin;
+        private GpioPinValue idleValue;
 
-        public bool DisableInterrupts { get; set; }
-        public bool GenerateCarrierFrequency { get; set; }
-        public long CarrierFrequency { get; set; }
+        public bool DisableInterrupts { get; set; } = false;
+        public bool GenerateCarrierFrequency { get; set; } = false;
+        public long CarrierFrequency { get; set; } = 0;
+        public GpioPinValue IdleValue { get => this.idleValue; set => this.pin.Write(this.idleValue = value); }
 
-        public SignalGenerator(int pinNumber, GpioPinValue initialValue) : this(GpioController.GetDefault(), pinNumber, initialValue) {
+        public SignalGenerator(int pinNumber) : this(GpioController.GetDefault(), pinNumber) {
 
         }
 
-        public SignalGenerator(GpioController gpioController, int pinNumber, GpioPinValue initialValue) {
+        public SignalGenerator(GpioController gpioController, int pinNumber) {
             if (!(gpioController.Provider is Gpio.Provider.GpioControllerApiWrapper p)) throw new NotSupportedException();
 
             this.gpioApi = p.Api.Implementation;
             this.pinNumber = pinNumber;
-
             this.pin = gpioController.OpenPin(pinNumber);
+
             this.pin.SetDriveMode(GpioPinDriveMode.Output);
-            this.pin.Write(initialValue);
+
+            this.IdleValue = GpioPinValue.Low;
         }
 
         public void Dispose() => this.pin.Dispose();
 
         public void Write(GpioPinValue value) => this.pin.Write(value);
 
-        public void Write(long[] buffer) => this.Write(buffer, 0, buffer.Length);
+        public void Write(TimeSpan[] buffer) => this.Write(buffer, 0, buffer.Length);
 
         [MethodImpl(MethodImplOptions.InternalCall)]
-        public extern void Write(long[] buffer, int offset, int count);
+        public extern void Write(TimeSpan[] buffer, int offset, int count);
     }
 
     public sealed class SignalCapture : IDisposable {
@@ -111,44 +116,36 @@ namespace GHIElectronics.TinyCLR.Devices.Signals {
         private readonly int pinNumber;
         private readonly GpioPin pin;
 
-        public bool DisableInterrupts { get; set; }
-        public TimeSpan Timeout { get; set; }
+        public bool DisableInterrupts { get; set; } = false;
+        public TimeSpan Timeout { get; set; } = TimeSpan.MaxValue;
+        public GpioPinDriveMode DriveMode { get => this.pin.GetDriveMode(); set => this.pin.SetDriveMode(value); }
 
-        public SignalCapture(int pinNumber, GpioPinDriveMode driveMode) : this(GpioController.GetDefault(), pinNumber, driveMode) {
+        public SignalCapture(int pinNumber) : this(GpioController.GetDefault(), pinNumber) {
 
         }
 
-        public SignalCapture(GpioController gpioController, int pinNumber, GpioPinDriveMode driveMode) {
+        public SignalCapture(GpioController gpioController, int pinNumber) {
             if (!(gpioController.Provider is Gpio.Provider.GpioControllerApiWrapper p)) throw new NotSupportedException();
-
-            switch (driveMode) {
-                case GpioPinDriveMode.Input:
-                case GpioPinDriveMode.InputPullUp:
-                case GpioPinDriveMode.InputPullDown: break;
-                default: throw new ArgumentException("Invalid drive mode.");
-            }
-
-            this.Timeout = TimeSpan.MaxValue;
 
             this.gpioApi = p.Api.Implementation;
             this.pinNumber = pinNumber;
-
             this.pin = gpioController.OpenPin(pinNumber);
-            this.pin.SetDriveMode(driveMode);
+
+            this.DriveMode = GpioPinDriveMode.Input;
         }
 
         public void Dispose() => this.pin.Dispose();
 
         public GpioPinValue Read() => this.pin.Read();
 
-        public int Read(out GpioPinValue initialState, long[] buffer) => this.Read(out initialState, buffer, 0, buffer.Length);
+        public int Read(out GpioPinValue initialState, TimeSpan[] buffer) => this.Read(out initialState, buffer, 0, buffer.Length);
 
-        public int Read(GpioPinValue waitForState, long[] buffer) => this.Read(waitForState, buffer, 0, buffer.Length);
-
-        [MethodImpl(MethodImplOptions.InternalCall)]
-        public extern int Read(out GpioPinValue initialState, long[] buffer, int offset, int count);
+        public int Read(GpioPinValue waitForState, TimeSpan[] buffer) => this.Read(waitForState, buffer, 0, buffer.Length);
 
         [MethodImpl(MethodImplOptions.InternalCall)]
-        public extern int Read(GpioPinValue waitforInitialState, long[] buffer, int offset, int count);
+        public extern int Read(out GpioPinValue initialState, TimeSpan[] buffer, int offset, int count);
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        public extern int Read(GpioPinValue waitForState, TimeSpan[] buffer, int offset, int count);
     }
 }
