84e418bf7808c0abc475a9a7abf4d9a3e66c5251
Palomino <dat.tran@ghielectronics.com>
2019-09-27 16:33:07 -0700

    fix feedback

--- GHIElectronics.TinyCLR.Update/Properties/AssemblyInfo.cs
+++ GHIElectronics.TinyCLR.Update/Properties/AssemblyInfo.cs
@@ -1,37 +1,13 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("GHIElectronics.TinyCLR.Update")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GHIElectronics.TinyCLR.Update")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
+[assembly: AssemblyTitle("GHIElectronics.TinyCLR.Data.Update")]
+[assembly: AssemblyDescription("TinyCLR OS Update library.")]
+[assembly: AssemblyCompany("GHI Electronics, LLC")]
+[assembly: AssemblyProduct("TinyCLR OS")]
+[assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2019")]
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2afa38ba-4dae-49f6-9f42-b8805bffb9cc")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+[assembly: Guid("2AFA38BA-4DAE-49F6-9F42-B8805BFFB9CC")]
 [assembly: AssemblyVersion("2.0.0.0")]
 [assembly: AssemblyFileVersion("2.0.0.10000")]
 [assembly: AssemblyInformationalVersion("2.0.0-preview1")]
