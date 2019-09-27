fe7d0e506115f8cd0de0d5468d0980c6e98fa02a
Palomino <dat.tran@ghielectronics.com>
2019-09-27 16:26:39 -0700

    change by feedbacks

--- GHIElectronics.TinyCLR.Update/BufferUpdater.cs
+++ GHIElectronics.TinyCLR.Update/BufferUpdater.cs
@@ -1,15 +1,53 @@
+using System;
 using System.Runtime.CompilerServices;
 
 namespace GHIElectronics.TinyCLR.Update {
-    public class BufferUpdater {
+    public static class BufferUpdater {
+        public static void PrepareFirmware(uint[] key, uint checksum) {
+            if (key == null) throw new ArgumentException("key can not be null", nameof(key));
+            NativePrepareFirmware(key, checksum);
+        }
+
+        public static void PrepareDeployment(uint[] key, uint checksum) {
+            if (key == null) throw new ArgumentException("key can not be null", nameof(key));
+            NativePrepareDeployment(key, checksum);
+        }
+
+        public static void LoadFirmware(byte[] data, long offset, long count) {
+            if (data == null)
+                throw new ArgumentException("data can not be null", nameof(data));
+
+            if (offset > count || offset + count > data.Length)
+                throw new ArgumentOutOfRangeException("out of range");
+
+            NativeLoadFirmware(data, offset, count);
+        }
+
+        public static void LoadDeployment(byte[] data, long offset, long count) {
+            if (data == null)
+                throw new ArgumentException("data can not be null", nameof(data));
+
+            if (offset > count || offset + count > data.Length)
+                throw new ArgumentOutOfRangeException("out of range");
+
+            NativeLoadDeployment(data, offset, count);
+        }
+
+        public static void Flash() => NativeFlash();
 
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
-        extern public static void LoadFirmware(byte[] data, uint[] key, uint checksum);
+        extern private static void NativePrepareFirmware(uint[] key, uint checksum);
 
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
-        extern public static void LoadDeployment(byte[] data, uint[] key, uint checksum);
+        extern private static void NativePrepareDeployment(uint[] key, uint checksum);
 
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
-        extern public static void Flash();
+        extern private static void NativeLoadFirmware(byte[] data, long offset, long count);
+
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        extern private static void NativeLoadDeployment(byte[] data, long offset, long count);
+
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        extern private static void NativeFlash();
     }
 }
--- GHIElectronics.TinyCLR.Update/FileUpdater.cs
+++ GHIElectronics.TinyCLR.Update/FileUpdater.cs
@@ -1,10 +1,28 @@
+using System;
 using System.IO;
 using System.Runtime.CompilerServices;
 
 namespace GHIElectronics.TinyCLR.Update {
-    public class FileUpdater {
+    public static class FileUpdater {
+        public static void PrepareDeployment(uint[] key, uint checksum) {
+            if (key == null) throw new ArgumentException("key can not be null", nameof(key));
+
+            NativePrepareDeployment(key, checksum);
+        }
+
+        public static void Flash(FileStream stream, long offset, long count) {
+            if (stream == null) throw new ArgumentException("stream can not be null", nameof(stream));
+
+            if (offset + count > stream.Length)
+                throw new ArgumentOutOfRangeException("out of range");
+
+            NativeFlash(stream, offset, count);
+        }
 
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
-        extern public static void FlashDeployment(FileStream stream, uint[] key, uint checksum);
+        extern private static void NativePrepareDeployment(uint[] key, uint checksum);
+
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        extern private static void NativeFlash(FileStream stream, long offset, long count);
     }
 }
