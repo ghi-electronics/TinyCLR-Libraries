dd42cc45e597aefbe351390a57320203a3058a04
John Brochue <john.brochue@ghielectronics.com>
2019-10-09 15:02:40 -0400

    Added secure/unsecure to memory manager

--- GHIElectronics.TinyCLR.Native/Memory.cs
+++ GHIElectronics.TinyCLR.Native/Memory.cs
@@ -1,20 +1,30 @@
-﻿using System;
+using System;
 using System.Runtime.CompilerServices;
 
 namespace GHIElectronics.TinyCLR.Native {
-    public static class Memory {
-        public static IntPtr Allocate(long length) => Memory.Allocate((IntPtr)length);
+    public class Memory {
+        private static Memory secure = new Memory(NativeApi.Find("GHIElectronics.TinyCLR.NativeApis.TinyCLR.SecureMemoryManager", NativeApiType.MemoryManager));
+        private static Memory unsecure = new Memory(NativeApi.Find("GHIElectronics.TinyCLR.NativeApis.TinyCLR.UnsecureMemoryManager", NativeApiType.MemoryManager));
+
+        private readonly IntPtr api;
+
+        private Memory(NativeApi api) => this.api = api.Implementation;
+
+        public static Memory SecureMemory => Memory.secure;
+        public static Memory UnsecureMemory => Memory.unsecure;
+
+        public IntPtr Allocate(long length) => this.Allocate((IntPtr)length);
 
         [MethodImpl(MethodImplOptions.InternalCall)]
-        public static extern IntPtr Allocate(IntPtr length);
+        public extern IntPtr Allocate(IntPtr length);
 
         [MethodImpl(MethodImplOptions.InternalCall)]
-        public static extern void Free(IntPtr ptr);
+        public extern void Free(IntPtr ptr);
 
         [MethodImpl(MethodImplOptions.InternalCall)]
-        public static extern void GetStats(out IntPtr used, out IntPtr free);
+        public extern void GetStats(out IntPtr used, out IntPtr free);
 
-        public static long UsedBytes { get { Memory.GetStats(out var used, out _); return used.ToInt64(); } }
-        public static long FreeBytes { get { Memory.GetStats(out _, out var free); return free.ToInt64(); } }
+        public long UsedBytes { get { this.GetStats(out var used, out _); return used.ToInt64(); } }
+        public long FreeBytes { get { this.GetStats(out _, out var free); return free.ToInt64(); } }
     }
 }
