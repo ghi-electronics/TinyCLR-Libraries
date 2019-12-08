15eab9c47500bcc5d1e68b0bf2dd749a148b7530
Palomino <dat.tran@ghielectronics.com>
2019-12-08 07:19:39 -0800

    add lock counter when access from 2 threads

--- GHIElectronics.TinyCLR.Media/Mpeg.cs
+++ GHIElectronics.TinyCLR.Media/Mpeg.cs
@@ -11,48 +11,52 @@ namespace GHIElectronics.TinyCLR.Media {
     public sealed class Mpeg {
         const int BLOCK_SIZE = 4 * 1024;
 
-        private byte[][] buffer;
-        private IntPtr[] unmanagedPtr;
-
         private FileStream streamToBuffer;
         private byte[] dataToBuffer;
 
         private bool isDecoding;
 
-        private int fifoIn;
-        private int fifoOut;
-        private int fifoCount;
-
         private Graphics screen;
-
-        private int bufferSize;
-        private int bufferCount;
 
         public class Setting {
             public int BufferSize { get; set; } = 2 * 1024 * 1024;
             public int BufferCount { get; set; } = 3;
-
             public Graphics Screen { get; set; }
         }
 
+        private class Fifo {
+            internal int bufferSize;
+            internal int bufferCount;
+
+            internal int fifoIn;
+            internal int fifoOut;
+            internal int fifoCount;
+
+            internal byte[][] buffer;
+            internal IntPtr[] unmanagedPtr;
+        }
+
+        private Fifo fifo;
+
         public Mpeg(Setting setting) {
+            this.fifo = new Fifo {
+                bufferSize = setting.BufferSize,
+                bufferCount = setting.BufferCount
+            };
 
-            this.bufferSize = setting.BufferSize;
-            this.bufferCount = setting.BufferCount;
+            this.fifo.buffer = new byte[this.fifo.bufferCount][];
+            this.fifo.unmanagedPtr = new IntPtr[this.fifo.bufferCount];
 
-            this.buffer = new byte[this.bufferCount][];
-            this.unmanagedPtr = new IntPtr[this.bufferCount];
-
-            for (var c = 0; c < this.bufferCount; c++) {
+            for (var c = 0; c < this.fifo.bufferCount; c++) {
 
                 if (DeviceInformation.DeviceName == "Sc20260") {
-                    var ptr = Memory.UnmanagedMemory.Allocate(this.bufferSize);
-                    this.buffer[c] = Memory.UnmanagedMemory.ToBytes(ptr, this.bufferSize);
+                    var ptr = Memory.UnmanagedMemory.Allocate(this.fifo.bufferSize);
+                    this.fifo.buffer[c] = Memory.UnmanagedMemory.ToBytes(ptr, this.fifo.bufferSize);
 
-                    this.unmanagedPtr[c] = ptr;
+                    this.fifo.unmanagedPtr[c] = ptr;
                 }
                 else {
-                    this.buffer[c] = new byte[this.bufferSize];
+                    this.fifo.buffer[c] = new byte[this.fifo.bufferSize];
                 }
             }
 
@@ -63,9 +67,11 @@ namespace GHIElectronics.TinyCLR.Media {
         }
 
         ~Mpeg() {
-            if (this.unmanagedPtr != null) {
-                for (var c = 0; c < this.bufferCount; c++) {
-                    Memory.UnmanagedMemory.Free(this.unmanagedPtr[c]);
+            this.isDecoding = false;
+
+            if (this.fifo.unmanagedPtr != null) {
+                for (var c = 0; c < this.fifo.bufferCount; c++) {
+                    Memory.UnmanagedMemory.Free(this.fifo.unmanagedPtr[c]);
                 }
             }
         }
@@ -111,20 +117,20 @@ namespace GHIElectronics.TinyCLR.Media {
                 if (!this.isDecoding)
                     break;
 
-                if (this.fifoCount == this.bufferCount) {
+                if (this.fifo.fifoCount == this.fifo.bufferCount) {
                     continue;
                 }
 
-                var lengthToBuffer = (int)((this.bufferSize < streamLength - i) ? this.bufferSize : (streamLength - i));
+                var lengthToBuffer = (int)((this.fifo.bufferSize < streamLength - i) ? this.fifo.bufferSize : (streamLength - i));
 
                 var block = lengthToBuffer / BLOCK_SIZE;
                 var remain = lengthToBuffer % BLOCK_SIZE;
 
                 var index = 0;
-                var id = this.fifoIn;
+                var id = this.fifo.fifoIn;
 
                 while (block > 0) {
-                    this.streamToBuffer.Read(this.buffer[id], index, BLOCK_SIZE);
+                    this.streamToBuffer.Read(this.fifo.buffer[id], index, BLOCK_SIZE);
                     index += BLOCK_SIZE;
                     block--;
 
@@ -134,16 +140,19 @@ namespace GHIElectronics.TinyCLR.Media {
                 }
 
                 if (remain > 0) {
-                    this.streamToBuffer.Read(this.buffer[id], index, remain);
+                    this.streamToBuffer.Read(this.fifo.buffer[id], index, remain);
 
                     i += remain;
                 }
 
-                this.fifoCount++;
-                this.fifoIn++;
+                lock (this.fifo) {
+                    this.fifo.fifoCount++;
+                }
 
-                if (this.fifoIn == this.bufferCount) {
-                    this.fifoIn = 0;
+                this.fifo.fifoIn++;
+
+                if (this.fifo.fifoIn == this.fifo.bufferCount) {
+                    this.fifo.fifoIn = 0;
                 }
             }
         }
@@ -158,20 +167,20 @@ namespace GHIElectronics.TinyCLR.Media {
                 if (!this.isDecoding)
                     break;
 
-                if (this.fifoCount == this.bufferCount) {
+                if (this.fifo.fifoCount == this.fifo.bufferCount) {
                     continue;
                 }
 
-                var lengthToBuffer = (int)((this.bufferSize < dataLength - i) ? this.bufferSize : (dataLength - i));
+                var lengthToBuffer = (int)((this.fifo.bufferSize < dataLength - i) ? this.fifo.bufferSize : (dataLength - i));
 
                 var block = lengthToBuffer / BLOCK_SIZE;
                 var remain = lengthToBuffer % BLOCK_SIZE;
 
                 var index = 0;
-                var id = this.fifoIn;
+                var id = this.fifo.fifoIn;
 
                 while (block > 0) {
-                    Array.Copy(this.dataToBuffer, i, this.buffer[id], index, BLOCK_SIZE);
+                    Array.Copy(this.dataToBuffer, i, this.fifo.buffer[id], index, BLOCK_SIZE);
 
                     index += BLOCK_SIZE;
                     i += BLOCK_SIZE;
@@ -180,16 +189,19 @@ namespace GHIElectronics.TinyCLR.Media {
                 }
 
                 if (remain > 0) {
-                    Array.Copy(this.dataToBuffer, i, this.buffer[id], index, remain);
+                    Array.Copy(this.dataToBuffer, i, this.fifo.buffer[id], index, remain);
 
                     i += remain;
                 }
 
-                this.fifoCount++;
-                this.fifoIn++;
+                lock (this.fifo) {
+                    this.fifo.fifoCount++;
+                }
 
-                if (this.fifoIn == this.bufferCount) {
-                    this.fifoIn = 0;
+                this.fifo.fifoIn++;
+
+                if (this.fifo.fifoIn == this.fifo.bufferCount) {
+                    this.fifo.fifoIn = 0;
                 }
             }
         }
@@ -199,32 +211,32 @@ namespace GHIElectronics.TinyCLR.Media {
             while (this.isDecoding) {
                 Thread.Sleep(1);
 
-                if (this.fifoCount == 0) {
+                if (this.fifo.fifoCount == 0) {
                     continue;
                 }
 
-                var id = this.fifoOut;
+                var id = this.fifo.fifoOut;
 
                 var startFrame = 0;
 
-                for (var i = 0; i < this.bufferSize - 4; i++) {
+                for (var i = 0; i < this.fifo.bufferSize - 4; i++) {
 
                     var jpegLength = 0;
                     var foundFrame = false;
 
-                    if (this.buffer[id][i] == (byte)'0'
-                    && this.buffer[id][i + 1] == (byte)'0'
-                    && this.buffer[id][i + 2] == (byte)'d'
-                    && this.buffer[id][i + 3] == (byte)'c') {
+                    if (this.fifo.buffer[id][i] == (byte)'0'
+                    && this.fifo.buffer[id][i + 1] == (byte)'0'
+                    && this.fifo.buffer[id][i + 2] == (byte)'d'
+                    && this.fifo.buffer[id][i + 3] == (byte)'c') {
                         i += 4;
 
-                        jpegLength = (this.buffer[id][i] | (this.buffer[id][i + 1] << 8) | (this.buffer[id][i + 2] << 16) | (this.buffer[id][i + 3] << 24));
+                        jpegLength = (this.fifo.buffer[id][i] | (this.fifo.buffer[id][i + 1] << 8) | (this.fifo.buffer[id][i + 2] << 16) | (this.fifo.buffer[id][i + 3] << 24));
 
                         i += 4;
 
                         startFrame = i;
 
-                        if (i + jpegLength > this.buffer[id].Length)
+                        if (i + jpegLength > this.fifo.buffer[id].Length)
                             break;
 
                         i += (jpegLength - 1);
@@ -238,7 +250,7 @@ namespace GHIElectronics.TinyCLR.Media {
 
                         var frameData = new byte[jpegLength];
 
-                        Array.Copy(this.buffer[id], startFrame, frameData, 0, frameData.Length);
+                        Array.Copy(this.fifo.buffer[id], startFrame, frameData, 0, frameData.Length);
 
                         using (var image = new Bitmap(frameData, BitmapImageType.Jpeg)) {
 
@@ -255,14 +267,16 @@ namespace GHIElectronics.TinyCLR.Media {
                             Thread.Sleep(50 - t2);
 
                     }
-
                 }
 
-                this.fifoCount--;
-                this.fifoOut++;
+                lock (this.fifo) {
+                    this.fifo.fifoCount--;
+                }
 
-                if (this.fifoOut == this.bufferCount)
-                    this.fifoOut = 0;
+                this.fifo.fifoOut++;
+
+                if (this.fifo.fifoOut == this.fifo.bufferCount)
+                    this.fifo.fifoOut = 0;
             }
         }
     }
