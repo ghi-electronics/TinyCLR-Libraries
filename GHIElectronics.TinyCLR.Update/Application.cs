a1c8067307143f284b4ab015e1a6caf2516e4f76
Dat <dat.tran@ghielectronics.com>
2020-01-13 12:10:33 -0500

    add IsLocked property

--- GHIElectronics.TinyCLR.Update/Application.cs
+++ GHIElectronics.TinyCLR.Update/Application.cs
@@ -6,9 +6,13 @@ using System.Threading;
 
 namespace GHIElectronics.TinyCLR.Update {
     public static class Application {
+        public static bool IsLocked => NativeIsLocked();
         public static void Lock() => NativeLock();
 
         [MethodImpl(MethodImplOptions.InternalCall)]
         private static extern void NativeLock();
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        private static extern bool NativeIsLocked();
     }
 }
