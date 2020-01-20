0709243f11f89eed3f1850e1101352d625b4323a
Dat <dat.tran@ghielectronics.com>
2020-01-20 16:13:57 -0500

    add onewire

---
+++ GHIElectronics.TinyCLR.Devices.Onewire/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GHIElectronics.TinyCLR.Devices.Onewire")]
+[assembly: AssemblyDescription("TinyCLR OS Onewire library.")]
+[assembly: AssemblyCompany("GHI Electronics, LLC")]
+[assembly: AssemblyProduct("TinyCLR OS")]
+[assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2020")]
+[assembly: ComVisible(false)]
+[assembly: Guid("F9E03CB8-A7FB-46CC-A3D7-6421E52972AA")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.10000")]
+[assembly: AssemblyInformationalVersion("2.0.0-preview2")]
+
