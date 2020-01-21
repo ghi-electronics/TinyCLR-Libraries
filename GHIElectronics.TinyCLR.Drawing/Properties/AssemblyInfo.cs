ef247a1037a46dbfa93667f11b6221372dbafcc9
Dat <dat.tran@ghielectronics.com>
2020-01-21 10:51:45 -0500

    update AssemblyFileVersion("2.0.0.20000")]

--- GHIElectronics.TinyCLR.Drawing/Properties/AssemblyInfo.cs
+++ GHIElectronics.TinyCLR.Drawing/Properties/AssemblyInfo.cs
@@ -9,5 +9,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("52A54E94-2FD8-4EEE-822C-C8C7F27AF423")]
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.10000")]
+[assembly: AssemblyFileVersion("2.0.0.20000")]
 [assembly: AssemblyInformationalVersion("2.0.0-preview2")]
--- GHIElectronics.TinyCLR.Media/Properties/AssemblyInfo.cs
+++ GHIElectronics.TinyCLR.Media/Properties/AssemblyInfo.cs
@@ -9,6 +9,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("2F1F64E0-5E96-46A4-A386-DE17AE01AD7D")]
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.10000")]
+[assembly: AssemblyFileVersion("2.0.0.20000")]
 [assembly: AssemblyInformationalVersion("2.0.0-preview2")]
 
