7a0a2dbb784996d656011b3015ad9633d8a4c5a6
Martin Calsyn <mcalsyn@pervasive.digital>
2020-03-28 19:23:13 -0700

    Text in texbox is misaligned
    The text in TextBox controls was misaligned and only half visible.  This change (suggested by Dat) fixes that by vertically centering the text within the textbox rectangle.

--- GHIElectronics.TinyCLR.UI/Controls/TextBox.cs
+++ GHIElectronics.TinyCLR.UI/Controls/TextBox.cs
@@ -53,7 +53,8 @@ namespace GHIElectronics.TinyCLR.UI.Controls {
 
             var txt = this.text;
             var diff = this._renderWidth - this.width;
-            var y = this._font.Height / 2;
+            // Place the centerline of the font at the center of the textbox
+            var y = (this.ActualHeight - this._font.Height) / 2;
 
             if (diff > 0) {
                 dc.DrawText(ref txt, this._font, b.Color, 0, y, this._renderWidth, this._font.Height, this.TextAlign, TextTrimming.CharacterEllipsis);
