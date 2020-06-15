b022b0350bf7ea7282ff8324c8adb9d3bb8f46c8
Dat <dat.tran@ghielectronics.com>
2020-06-15 11:54:11 -0400

    to rc1

--- GHIElectronics.TinyCLR.Drawing/Properties/AssemblyInfo.cs
+++ GHIElectronics.TinyCLR.Drawing/Properties/AssemblyInfo.cs
@@ -9,5 +9,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("52A54E94-2FD8-4EEE-822C-C8C7F27AF423")]
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.60000")]
-[assembly: AssemblyInformationalVersion("2.0.0-preview6")]
+[assembly: AssemblyFileVersion("2.0.0.70000")]
+[assembly: AssemblyInformationalVersion("2.0.0-rc1")]
