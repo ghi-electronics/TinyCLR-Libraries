ab9a14841ba5150d14b9eadb7bf709f384ba551d
Dat <dat.tran@ghielectronics.com>
2020-10-26 13:11:00 -0400

    change AssemblyInfo to 210

--- GHIElectronics.TinyCLR.Devices.Onewire/Properties/AssemblyInfo.cs
+++ GHIElectronics.TinyCLR.Devices.Onewire/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2020")]
 [assembly: ComVisible(false)]
 [assembly: Guid("F9E03CB8-A7FB-46CC-A3D7-6421E52972AA")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.9000")]
-[assembly: AssemblyInformationalVersion("2.0.0-rtw")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.1000")]
+[assembly: AssemblyInformationalVersion("2.1.0-preview1")]
 
