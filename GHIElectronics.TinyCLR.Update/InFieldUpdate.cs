b4ec5ae89061f79060cc24f35f9de486ab6ac632
Dat <dat.tran@ghielectronics.com>
2020-12-10 13:45:54 -0500

    clean

--- GHIElectronics.TinyCLR.Update/InFieldUpdate.cs
+++ GHIElectronics.TinyCLR.Update/InFieldUpdate.cs
@@ -1,10 +1,8 @@
 using System;
-using System.Collections;
 using System.Diagnostics;
 using System.IO;
 using System.Net.Sockets;
 using System.Runtime.CompilerServices;
-using System.Text;
 using System.Threading;
 using GHIElectronics.TinyCLR.Devices.Storage;
 using GHIElectronics.TinyCLR.Pins;
@@ -35,14 +33,15 @@ namespace GHIElectronics.TinyCLR.Update {
         private Stream applicationStream;
         private uint bufferSize;
 
+        public TimeSpan DataTimeOut { get; set; } = TimeSpan.FromSeconds(5);
 
-        const uint BUFFER_SIZE = 4 * 1024;
+        const uint BUFFER_SIZE = 1 * 1024;
 
-        public InFieldUpdate(Stream applicationStream, bool useExternalFlash = false) : this(null, applicationStream, useExternalFlash) {
+        public InFieldUpdate(Stream applicationStream, bool useExternalFlash = false) : this(applicationStream, null, useExternalFlash) {
 
         }
 
-        public InFieldUpdate(Stream firmwareStream, Stream applicationStream, bool useExternalFlash = false) {
+        public InFieldUpdate(Stream applicationStream, Stream firmwareStream, bool useExternalFlash = false) {
             if (firmwareStream != null) {
                 this.mode |= Mode.Firmware;
             }
@@ -63,66 +62,29 @@ namespace GHIElectronics.TinyCLR.Update {
             this.applicationStream = applicationStream;
             this.useExternalFlash = useExternalFlash;
 
-            if (firmwareStream != null) {
-                if (firmwareStream is FileStream firmwareFileStream) {
-                    if ((applicationStream) == null)
-                        this.NativeInFieldUpdate(firmwareFileStream, null, useExternalFlash);
-                    else {
-                        if (applicationStream is FileStream applicationFileStream) {
-                            this.NativeInFieldUpdate(firmwareFileStream, applicationFileStream, useExternalFlash);
-                        }
-                        else {
-                            throw new ArgumentException("");
-                        }
-                    }
-
-                }
-                else if (firmwareStream is MemoryStream firmwareMemoryStream) {
-
-                    var firmwareBuffer = firmwareMemoryStream.ToArray();
-
-                    if ((applicationStream) == null)
-                        this.NativeInFieldUpdate(firmwareBuffer, null);
-                    else {
-                        if (applicationStream is MemoryStream applicationMemoryStream) {
-
-                            var applicationBuffer = applicationMemoryStream.ToArray();
-
-                            this.NativeInFieldUpdate(firmwareBuffer, applicationBuffer);
-                        }
-                        else {
-                            throw new ArgumentException("");
-                        }
-                    }
-
-                }
-                else if (firmwareStream is NetworkStream firmwareNetworkStream) {
-                    if ((applicationStream) == null) {
-                        this.NativeInFieldUpdate(firmwareNetworkStream, null);
-                    }
-                    else {
-                        if (applicationStream is NetworkStream applicationNetworkStream) {
-                            this.NativeInFieldUpdate(firmwareNetworkStream, applicationNetworkStream);
-                        }
-                        else {
-                            throw new ArgumentException("");
-                        }
-                    }
-                }
+            if (useExternalFlash) {
+                this.NativeInFieldUpdate(applicationStream, firmwareStream, useExternalFlash);
             }
             else {
-                if (applicationStream is FileStream applicationFileStream) {
-                    this.NativeInFieldUpdate(null, applicationFileStream, useExternalFlash);
-                }
-                else if (applicationStream is MemoryStream applicationMemoryStream) {
+                if (firmwareStream != null && !(firmwareStream is MemoryStream))
+                    throw new ArgumentException("Firmware has to be Memory stream or use external flash.");
 
-                    var applicationBuffer = applicationMemoryStream.ToArray();
+                byte[] applicationBuffer = null;
+                byte[] firmwareBuffer = null;
 
-                    this.NativeInFieldUpdate(null, applicationBuffer);
+                if (applicationStream is MemoryStream app) {
+                    applicationBuffer = app.ToArray();
                 }
-                else if (applicationStream is NetworkStream applicationNetworkStream) {
-                    this.NativeInFieldUpdate(null, applicationNetworkStream);
+
+                if (firmwareStream is MemoryStream fw) {
+                    firmwareBuffer = fw.ToArray();
                 }
+
+                if (applicationBuffer != null)
+                    this.NativeInFieldUpdate(applicationBuffer, firmwareBuffer);
+                else
+                    this.NativeInFieldUpdate(applicationStream, firmwareStream, useExternalFlash);
+
             }
 
             if (useExternalFlash) {
@@ -130,105 +92,23 @@ namespace GHIElectronics.TinyCLR.Update {
 
                 this.cacheStorage.Provider.Open();
 
-                if (this.cacheStorage.Descriptor != null && this.cacheStorage.Descriptor.RegionSizes != null)
-                    this.bufferSize = (uint)this.cacheStorage.Descriptor.RegionSizes[0];
-                else
-                    this.bufferSize = BUFFER_SIZE;
+                this.bufferSize = BUFFER_SIZE;
 
                 this.buffer = new byte[this.bufferSize];
             }
-
-
-
         }
 
-        //public InFieldUpdate(byte[] firmwareBuffer, byte[] applicationBuffer) {
-        //    if (firmwareBuffer == null && applicationBuffer == null)
-        //        throw new ArgumentNullException();
-
-        //    this.NativeInFieldUpdate(firmwareBuffer, applicationBuffer);
-
-        //    if (firmwareBuffer != null)
-        //        this.mode |= Mode.Firmware;
-
-        //    if (applicationBuffer != null)
-        //        this.mode |= Mode.Application;
-
-        //    this.mode |= Mode.Firmware | Mode.Application;
-        //}
-
-        //public InFieldUpdate(FileStream stream) {
-        //    if (stream == null) throw new ArgumentNullException(nameof(stream));
-
-        //    this.NativeInFieldUpdate(stream);
-
-        //    this.mode |= Mode.Application;
-        //}
 
         public void AuthenticateFirmware(out uint version) {
             if ((this.mode & Mode.Firmware) != Mode.Firmware)
                 throw new ArgumentNullException();
 
             if (this.useExternalFlash) {
-                if (this.firmwareStream is NetworkStream) {
-                    //Todo
-                }
-                else if (this.firmwareStream is MemoryStream) {
-                    //No thing to do
-                }
-                else if (this.firmwareStream is FileStream) {
-                    this.firmwareStream.Seek(0, SeekOrigin.Begin);
-
-                    var block = this.firmwareStream.Length / this.bufferSize;
-                    var remain = this.firmwareStream.Length % this.bufferSize;
-                    var idx = 0U;
-                    var address = FirmwareAddressOffet;
-
-                    // erase
-                    var b = remain > 0 ? (block + 1) : block;
-
-                    while (b > 0) {
-                        var size = (this.firmwareStream.Length - idx) > this.bufferSize ? this.bufferSize : (this.firmwareStream.Length - idx);
-
-                        if (!this.cacheStorage.Provider.IsErased(address, (int)size)) {
-                            Debug.WriteLine("Erasing..." + address);
-                            this.cacheStorage.Provider.Erase(address, (int)size, TimeSpan.FromSeconds(1));
-                        }
-
-                        b--;
-                        idx += (uint)size;
-                        address += (uint)size;
-
-
-
-
-                    }
-
-                    idx = 0;
-                    address = FirmwareAddressOffet;
-
-                    b = remain > 0 ? (block + 1) : block;
-
-                    while (b > 0) {
-                        var r = (this.firmwareStream.Length - idx) > this.bufferSize ? this.bufferSize : (this.firmwareStream.Length - idx);
-                        var size = this.firmwareStream.Read(this.buffer, 0, (int)r);
-
-                        if (size > 0) {
-                            this.cacheStorage.Provider.Write(address, size, this.buffer, 0, TimeSpan.FromSeconds(1));
-
-                            Debug.WriteLine("Writting..." + idx + ", address: " + address);
-                        }
-
-                        b--;
-                        idx += (uint)size;
-                        address += (uint)size;
-
-
-                        Thread.Sleep(1);
-                    }
-
-                    this.NativeSetFirmwareSize((uint)this.firmwareStream.Length);
-                }
+                var totalBufferred = this.BufferingData(this.firmwareStream, FirmwareAddressOffet, FirmwareMaxSize);
+                if (totalBufferred > 0)
+                    this.NativeSetFirmwareSize((uint)totalBufferred);
+                else
+                    throw new InvalidOperationException("Data not available.");
             }
 
             version = this.NativeAuthenticateFirmware();
@@ -241,59 +121,12 @@ namespace GHIElectronics.TinyCLR.Update {
                 throw new ArgumentNullException();
 
             if (this.useExternalFlash) {
-                if (this.applicationStream is NetworkStream) {
-                    //Todo
-                }
-                else if (this.applicationStream is MemoryStream) {
-                    //No thing to do
-                }
-                else if (this.applicationStream is FileStream) {
-                    this.applicationStream.Seek(0, SeekOrigin.Begin);
+                var totalBufferred = this.BufferingData(this.applicationStream, ApplicationAddressOffet, ApplicationMaxSize);
 
-                    var block = this.applicationStream.Length / this.bufferSize;
-                    var remain = this.applicationStream.Length % this.bufferSize;
-                    var idx = 0U;
-                    var address = ApplicationAddressOffet;
-
-                    // erase
-                    var b = remain > 0 ? (block + 1) : block;
-
-                    while (b > 0) {
-                        var size = (this.applicationStream.Length - idx) > this.bufferSize ? this.bufferSize : (this.applicationStream.Length - idx);
-
-                        if (!this.cacheStorage.Provider.IsErased(address, (int)size)) {
-                            Debug.WriteLine("Erasing app..." + address);
-                            this.cacheStorage.Provider.Erase(address, (int)size, TimeSpan.FromSeconds(1));
-                        }
-
-                        b--;
-                        idx += (uint)size;
-                        address += (uint)size;
-
-                    }
-
-                    idx = 0;
-                    address = ApplicationAddressOffet;
-
-                    b = remain > 0 ? (block + 1) : block;
-
-                    while (b > 0) {
-                        var r = (this.applicationStream.Length - idx) > this.bufferSize ? this.bufferSize : (this.applicationStream.Length - idx);
-                        var size = this.applicationStream.Read(this.buffer, 0, (int)r);
-
-                        if (size > 0) {
-                            Debug.WriteLine("Writting app..." + address);
-                            this.cacheStorage.Provider.Write(address, size, this.buffer, 0, TimeSpan.FromSeconds(1));
-                        }
-
-                        b--;
-                        idx += (uint)size;
-                        address += (uint)size;
-                    }
-
-                    this.NativeSetApplicationSize((uint)this.applicationStream.Length);
-                }
-
+                if (totalBufferred > 0)
+                    this.NativeSetApplicationSize((uint)totalBufferred);
+                else
+                    throw new InvalidOperationException("Data not available.");
             }
 
             version = this.NativeAuthenticateApplication(this.ApplicationKey);
@@ -301,11 +134,23 @@ namespace GHIElectronics.TinyCLR.Update {
 
         public void FlashAndReset() {
             if (this.mode != Mode.None) {
-                if ((this.mode & Mode.Firmware) == Mode.Firmware)
-                    this.AuthenticateFirmware(out var fwVer);
+                if ((this.mode & Mode.Firmware) == Mode.Firmware) {
+                    if (this.useExternalFlash) {
+                        var fwVer = this.NativeAuthenticateFirmware();
+                    }
+                    else {
+                        this.AuthenticateFirmware(out var fwVer);
+                    }
+                }
 
-                if ((this.mode & Mode.Application) == Mode.Application)
-                    this.AuthenticateApplication(out var appVer);
+                if ((this.mode & Mode.Application) == Mode.Application) {
+                    if (this.useExternalFlash) {
+                        var appVer = this.NativeAuthenticateApplication(this.ApplicationKey);
+                    }
+                    else {
+                        this.AuthenticateApplication(out var appVer);
+                    }
+                }
 
                 this.NativeFlashAndReset();
             }
@@ -313,17 +158,57 @@ namespace GHIElectronics.TinyCLR.Update {
             throw new ArgumentNullException();
         }
 
-        [MethodImpl(MethodImplOptions.InternalCall)]
-        private extern void NativeInFieldUpdate(byte[] firmwareBuffer, byte[] applicationBuffer);
+        private int BufferingData(Stream stream, uint address, uint maxSize) {
+            var totalRead = 0;
+            var doRead = true;
+
+            if (stream is FileStream || stream is MemoryStream) {
+                stream.Seek(0, SeekOrigin.Begin);
+            }
+
+            while (doRead) {
+                var t = DateTime.Now.Ticks;
+
+                var read = 0;
+
+                while (read < (int)this.bufferSize) {
+                    read += stream.Read(this.buffer, read, (int)this.bufferSize - read);
+                    var delta = DateTime.Now.Ticks - t;
+
+                    if (read < 0 || (delta > this.DataTimeOut.Ticks)) {
+                        doRead = false;
+
+                        break;
+                    }
+                }
+
+                if (read > 0) {
+                    totalRead += read;
+
+                    if (totalRead > maxSize) {
+                        throw new ArgumentException("Invalid firmware.");
+                    }
+
+                    if (!this.cacheStorage.Provider.IsErased(address, read)) {
+                        Debug.WriteLine("Erasing address: " + address);
+                        this.cacheStorage.Provider.Erase(address, read, this.DataTimeOut);
+                    }
+
+                    Debug.WriteLine("Writting address: " + address + ", size " + read);
+                    this.cacheStorage.Provider.Write(address, read, this.buffer, 0, this.DataTimeOut);
+
+                    address += (uint)read;
+                }
+            }
+
+            return totalRead;
+        }
 
         [MethodImpl(MethodImplOptions.InternalCall)]
-        private extern void NativeInFieldUpdate(FileStream firmwareStream, FileStream applicationStream, bool useExternalFlash);
+        private extern void NativeInFieldUpdate(byte[] applicationBuffer, byte[] firmwareBuffer);
 
         [MethodImpl(MethodImplOptions.InternalCall)]
-        private extern void NativeInFieldUpdate(NetworkStream firmwareStream, NetworkStream applicationStream);
-
-        //[MethodImpl(MethodImplOptions.InternalCall)]
-        //private extern void NativeInFieldUpdate(FileStream stream);
+        private extern void NativeInFieldUpdate(Stream applicationStream, Stream firmwareStream, bool useExternalFlash);
 
         [MethodImpl(MethodImplOptions.InternalCall)]
         private extern uint NativeAuthenticateFirmware();
