5f317158ef5b3423d163ebbf6912a134c32158ab
Andre Marschalek <andre.marschalek@outlook.com>
2021-03-17 10:21:39 +0100

    Revert "TextBox has no borders #828"
    This reverts commit e593de093bb83f972458dc5bcca74e735f8b973c.

--- GHIElectronics.TinyCLR.UI/Controls/TextBox.cs
+++ GHIElectronics.TinyCLR.UI/Controls/TextBox.cs
@@ -11,9 +11,7 @@ namespace GHIElectronics.TinyCLR.UI.Controls {
 
     public class TextBox : Control {
         private string text = string.Empty;
-        private Color bordercolor = Colors.Black;
-        private ushort borderthickness = 1, paddingx, paddingy;
-        private int width, height;
+        private int width;
 
         public TextBox() => this.Background = new SolidColorBrush(Colors.White);
 
@@ -35,34 +33,6 @@ namespace GHIElectronics.TinyCLR.UI.Controls {
             }
         }
 
-        public Color BorderColor {
-            get => this.bordercolor;
-            set {
-                this.bordercolor = value;
-
-                this.InvalidateMeasure();
-            }
-        }
-
-        public ushort BorderThickness {
-            get => this.borderthickness;
-            set {
-                this.borderthickness = value;
-
-                this.InvalidateMeasure();
-            }
-        }
-
-        public ushort PaddingX {
-            get => this.paddingx;
-            set => this.paddingx = value;
-        }
-
-        public ushort PaddingY {
-            get => this.paddingy;
-            set => this.paddingy = value;
-        }
-
         internal bool ForOnScreenKeyboard { get; set; }
 
         protected override void OnTouchUp(TouchEventArgs e) {
@@ -77,13 +47,7 @@ namespace GHIElectronics.TinyCLR.UI.Controls {
         protected override void MeasureOverride(int availableWidth, int availableHeight, out int desiredWidth, out int desiredHeight) {
             this._font.ComputeExtent(this.text, out desiredWidth, out desiredHeight);
 
-            desiredWidth = this._font.MaxWidth + (this.PaddingY * 2) + (this.BorderThickness * 2);
-            desiredHeight = this._font.Height + (this.PaddingY * 2) + (this.BorderThickness * 2);
-        }
-
-        protected override void ArrangeOverride(int arrangeWidth, int arrangeHeight) {
-            this.width = arrangeWidth;
-            this.height = arrangeHeight;
+            this.width = desiredWidth;
         }
 
         public override void OnRender(DrawingContext dc) {
@@ -92,24 +56,16 @@ namespace GHIElectronics.TinyCLR.UI.Controls {
             base.OnRender(dc);
 
             var txt = this.text;
-            //var diff = this._renderWidth - this.width;
+            var diff = this._renderWidth - this.width;
             // Place the centerline of the font at the center of the textbox
-            //var y = (this.ActualHeight - this._font.Height) / 2;
-            var x = this.BorderThickness + this.PaddingX;
-            var y = this.BorderThickness + this.PaddingY;
-            var w = this.width - (this.BorderThickness * 2) - (this.PaddingX * 2);
+            var y = (this.ActualHeight - this._font.Height) / 2;
 
-            if (this.BorderThickness > 0) {
-                dc.DrawRectangle(this.Background, new Pen(this.BorderColor, this.BorderThickness), 0, 0, this.width, this.height);
+            if (diff > 0) {
+                dc.DrawText(ref txt, this._font, b.Color, 0, y, this._renderWidth, this._font.Height, this.TextAlign, TextTrimming.CharacterEllipsis);
             }
-
-            //if (diff > 0) {
-            //    dc.DrawText(ref txt, this._font, b.Color, 0, y, this._renderWidth, this._font.Height, this.TextAlign, TextTrimming.CharacterEllipsis);
-            //}
-            //else {
-            //    dc.DrawText(ref txt, this._font, b.Color, diff, y, this._renderWidth + this.width, this._font.Height, this.TextAlign, TextTrimming.CharacterEllipsis);
-            //}
-            dc.DrawText(ref txt, this._font, b.Color, x, y, w, this._font.Height, this.TextAlign, TextTrimming.CharacterEllipsis);
+            else {
+                dc.DrawText(ref txt, this._font, b.Color, diff, y, this._renderWidth + this.width, this._font.Height, this.TextAlign, TextTrimming.CharacterEllipsis);
+            }
         }
     }
 }
