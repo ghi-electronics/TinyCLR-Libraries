cbeafa01e0ecc96c8278f43a373dc15691cea290
Dat Tran <dat.tran@ghielectronics.com>
2021-04-07 13:17:22 -0400

    add gpio low level allow change gpio alternate function

---
+++ GHIElectronics.TinyCLR.Devices.Gpio.LowLevel/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GHIElectronics.TinyCLR.Devices.Gpio.LowLevel")]
+[assembly: AssemblyDescription("TinyCLR OS GPIO library.")]
+[assembly: AssemblyCompany("GHI Electronics, LLC")]
+[assembly: AssemblyProduct("TinyCLR OS")]
+[assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2021")]
+[assembly: ComVisible(false)]
+[assembly: Guid("F86BE83A-AEEB-4BB7-87EB-FD3745B3F4D2")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.4000")]
+[assembly: AssemblyInformationalVersion("2.1.0-preview4")]
+
