86a10c39070cd2b645599054d8878884004fa78d
Dat Tran <dat.tran@ghielectronics.com>
2022-05-18 11:02:58 -0400

    to v2.2.0.4200 (back from 5000)

--- GHIElectronics.TinyCLR.Devices.Modbus/Properties/AssemblyInfo.cs
+++ GHIElectronics.TinyCLR.Devices.Modbus/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2022")]
 [assembly: ComVisible(false)]
 [assembly: Guid("19886B6D-0A32-42B4-94A0-82614B34ACEF")]
-[assembly: AssemblyVersion("2.2.0.5000")]
-[assembly: AssemblyFileVersion("2.2.0.5000")]
-[assembly: AssemblyInformationalVersion("2.2.0.5000")]
+[assembly: AssemblyVersion("2.2.0.4200")]
+[assembly: AssemblyFileVersion("2.2.0.4200")]
+[assembly: AssemblyInformationalVersion("2.2.0.4200")]
 
