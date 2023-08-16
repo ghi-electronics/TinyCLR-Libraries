2ccd8c2c1c2bfbe4a8cd0585a560dda4f697424d
Dat Tran <dat.tran@ghielectronics.com>
2023-08-16 12:05:37 -0400

    to 2.2.0.6000

--- GHIElectronics.TinyCLR.Devices.Camera/Properties/AssemblyInfo.cs
+++ GHIElectronics.TinyCLR.Devices.Camera/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2022")]
 [assembly: ComVisible(false)]
 [assembly: Guid("104fa29f-f58b-4c4f-a9b1-a1527fd6a3ef")]
-[assembly: AssemblyVersion("2.2.0.5000")]
-[assembly: AssemblyFileVersion("2.2.0.5000")]
-[assembly: AssemblyInformationalVersion("2.2.0.5000")]
+[assembly: AssemblyVersion("2.2.0.6000")]
+[assembly: AssemblyFileVersion("2.2.0.6000")]
+[assembly: AssemblyInformationalVersion("2.2.0.6000")]
 
