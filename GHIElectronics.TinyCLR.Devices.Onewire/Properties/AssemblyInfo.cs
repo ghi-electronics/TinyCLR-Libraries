99a2939353ca93eef9e41f60567d3697cabb0ce1
Dat Tran <dat.tran@ghielectronics.com>
2024-03-18 10:52:26 -0400

    to v2.2.1.1000

--- GHIElectronics.TinyCLR.Devices.Onewire/Properties/AssemblyInfo.cs
+++ GHIElectronics.TinyCLR.Devices.Onewire/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2022")]
 [assembly: ComVisible(false)]
 [assembly: Guid("F9E03CB8-A7FB-46CC-A3D7-6421E52972AA")]
-[assembly: AssemblyVersion("2.2.1")]
-[assembly: AssemblyFileVersion("2.2.1")]
-[assembly: AssemblyInformationalVersion("2.2.1")]
+[assembly: AssemblyVersion("2.2.1.1000")]
+[assembly: AssemblyFileVersion("2.2.1.1000")]
+[assembly: AssemblyInformationalVersion("2.2.1.1000")]
 
