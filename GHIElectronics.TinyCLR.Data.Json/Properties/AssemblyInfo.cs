a9c94720ea7d2b9bcd8b56d696c7138b83cd3ac1
Dat Tran <dat.tran@ghielectronics.com>
2024-03-19 13:15:49 -0400

    to 2.2.1.2000

--- GHIElectronics.TinyCLR.Data.Json/Properties/AssemblyInfo.cs
+++ GHIElectronics.TinyCLR.Data.Json/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2022")]
 [assembly: ComVisible(false)]
 [assembly: Guid("4B7A2C87-ABC9-4B99-A500-DB147F909945")]
-[assembly: AssemblyVersion("2.2.1.1000")]
-[assembly: AssemblyFileVersion("2.2.1.1000")]
-[assembly: AssemblyInformationalVersion("2.2.1.1000")]
+[assembly: AssemblyVersion("2.2.1.2000")]
+[assembly: AssemblyFileVersion("2.2.1.2000")]
+[assembly: AssemblyInformationalVersion("2.2.1.2000")]
 
--- GHIElectronics.TinyCLR.Devices.Camera/Properties/AssemblyInfo.cs
+++ GHIElectronics.TinyCLR.Devices.Camera/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2022")]
 [assembly: ComVisible(false)]
 [assembly: Guid("104fa29f-f58b-4c4f-a9b1-a1527fd6a3ef")]
-[assembly: AssemblyVersion("2.2.1.1000")]
-[assembly: AssemblyFileVersion("2.2.1.1000")]
-[assembly: AssemblyInformationalVersion("2.2.1.1000")]
+[assembly: AssemblyVersion("2.2.1.2000")]
+[assembly: AssemblyFileVersion("2.2.1.2000")]
+[assembly: AssemblyInformationalVersion("2.2.1.2000")]
 
