a14dea6e0d35c1786522a73347aaad4fb39d24ae
Dat Tran <dat.tran@ghielectronics.com>
2024-07-12 12:04:05 -0400

    add Copyright

--- GHIElectronics.TinyCLR.EthernetIP.Adapter/CipInstance.cs
+++ GHIElectronics.TinyCLR.EthernetIP.Adapter/CipInstance.cs
@@ -1,3 +1,7 @@
+// Copyright (c) 2024 GHI Electronics LLC
+// Based on OpENer library: Copyright (c) 2009, Rockwell Automation, Inc. ALL RIGHTS RESERVED.
+// EtherNet/IP is a trademark of ODVA, Inc.
+
 using System;
 using System.Runtime.CompilerServices;
 
