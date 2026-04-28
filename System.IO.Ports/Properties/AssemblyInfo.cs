e36b5731c2a95b2c70b17fcdbab43b09e557519a
Dat Tran <dat.tran@ghielectronics.com>
2026-04-28 15:28:17 -0400

    add serialport

---
+++ System.IO.Ports/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("System.IO.Ports")]
+[assembly: AssemblyDescription("System.IO.Ports compatibility library for TinyCLR OS.")]
+[assembly: AssemblyCompany("GHI Electronics, LLC")]
+[assembly: AssemblyProduct("TinyCLR OS")]
+[assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2026")]
+[assembly: ComVisible(false)]
+[assembly: Guid("420E1AF4-D5F6-4988-B7CB-51D80A87B7C5")]
+[assembly: AssemblyVersion("2.5.0.1000")]
+[assembly: AssemblyFileVersion("2.5.0.1000")]
+[assembly: AssemblyInformationalVersion("2.5.0.1000")]
