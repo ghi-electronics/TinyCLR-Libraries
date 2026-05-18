fdd21cc458bb9613f987d03e59ec414f10576755
Dat Tran <dat.tran@ghielectronics.com>
2026-05-18 13:23:28 -0400

    add GHIElectronics.TinyCLR.Collections (for hashset)

---
+++ GHIElectronics.TinyCLR.Collections/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GHIElectronics.TinyCLR.Collections")]
+[assembly: AssemblyDescription("TinyCLR OS Collections library (HashSet/ISet).")]
+[assembly: AssemblyCompany("GHI Electronics, LLC")]
+[assembly: AssemblyProduct("TinyCLR OS")]
+[assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2026")]
+[assembly: ComVisible(false)]
+[assembly: Guid("9aed2abd-2b23-4ad9-96de-65769172ff32")]
+[assembly: AssemblyVersion("3.0.0.1000")]
+[assembly: AssemblyFileVersion("3.0.0.1000")]
+[assembly: AssemblyInformationalVersion("3.0.0.1000-prerelease")]
