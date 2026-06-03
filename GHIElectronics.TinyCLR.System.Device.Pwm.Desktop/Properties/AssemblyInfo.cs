1a60716d51a628ca2248cc03c15d7958465a074d
Dat Tran <dat.tran@ghielectronics.com>
2026-06-03 16:34:47 -0400

    Fix missing SerialPort and PWM in API website

---
+++ GHIElectronics.TinyCLR.System.Device.Pwm.Desktop/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("System.Device.Pwm")]
+[assembly: AssemblyDescription("System.Device.Pwm compatibility library for TinyCLR OS.")]
+[assembly: AssemblyCompany("GHI Electronics, LLC")]
+[assembly: AssemblyProduct("TinyCLR OS")]
+[assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2026")]
+[assembly: ComVisible(false)]
+[assembly: Guid("E16C0DCB-0BF1-4F44-9FEA-952A85050902")]
+[assembly: AssemblyVersion("3.0.0.1000")]
+[assembly: AssemblyFileVersion("3.0.0.1000")]
+[assembly: AssemblyInformationalVersion("3.0.0.1000-prerelease")]
---
+++ GHIElectronics.TinyCLR.System.IO.Ports/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("System.IO.Ports")]
+[assembly: AssemblyDescription("System.IO.Ports (SerialPort) compatibility library for TinyCLR OS.")]
+[assembly: AssemblyCompany("GHI Electronics, LLC")]
+[assembly: AssemblyProduct("TinyCLR OS")]
+[assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2026")]
+[assembly: ComVisible(false)]
+[assembly: Guid("A62A922D-3777-49D9-AA9F-690B41B76871")]
+[assembly: AssemblyVersion("3.0.0.1000")]
+[assembly: AssemblyFileVersion("3.0.0.1000")]
+[assembly: AssemblyInformationalVersion("3.0.0.1000-prerelease")]
