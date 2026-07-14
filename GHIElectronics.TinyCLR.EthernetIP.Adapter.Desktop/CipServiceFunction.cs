5397c9d98182582a01c2f6bbb39f0cb14739e43f
Dat Tran <dat.tran@ghielectronics.com>
2026-07-14 14:59:11 -0400

    remove SHIM-Desktop

--- GHIElectronics.TinyCLR.EthernetIP.Adapter.Desktop/CipServiceFunction.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2024 GHI Electronics LLC
-// Based on OpENer library: Copyright (c) 2009, Rockwell Automation, Inc. ALL RIGHTS RESERVED.
-// EtherNet/IP is a trademark of ODVA, Inc.
-
-// See the device-side file with the same name for the rationale. The
-// CipServiceFunctionCode enum was a byte-for-byte duplicate of CIPServiceCode;
-// InsertService now uses CIPServiceCode for both serviceCode and handlerCode.
-// Empty stub retained for csproj reference parity until both csproj files drop
-// the CipServiceFunction.cs Compile entry.
